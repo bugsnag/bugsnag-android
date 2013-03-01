158d4c16283fe6c63ec313e5da60e381f0c24894
James Smith <james@loopj.com>
2013-03-01 12:53:06 -0800

    Inheriting from client was dumb, lets not do that

--- src/main/java/com/bugsnag/android/Bugsnag.java
+++ src/main/java/com/bugsnag/android/Bugsnag.java
@@ -1,29 +1,109 @@
 package com.bugsnag.android;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
 import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+
+import com.bugsnag.Client;
+import com.bugsnag.Error;
 import com.bugsnag.MetaData;
+import com.bugsnag.Notification;
 
 public class Bugsnag {
+    private static final String PREFS_NAME = "Bugsnag";
+    private static final String UNSENT_ERROR_PATH = "/bugsnag-errors/";
+
+    private static Context applicationContext;
+    private static String packageName;
+    private static String packageVersion;
+    private static String cachePath;
+    private static Logger logger;
     private static Client client;
 
     public static void register(Context androidContext, String apiKey) {
-        client = new Client(androidContext, apiKey);
+        // Create a logger
+        logger = new Logger();
+
+        // Create the bugsnag client
+        client = new Client(apiKey);
+        client.setLogger(logger);
+
+        // Get the application context, many things need this
+        applicationContext = androidContext.getApplicationContext();
+        packageName = getPackageName();
+        packageVersion = getPackageVersion(packageName);
+        cachePath = prepareCachePath();
+
+        // Set common meta-data
+        client.setUserId(getUUID());
+        client.setOsVersion(android.os.Build.VERSION.RELEASE);
+        client.setAppVersion(packageVersion);
+        client.setProjectPackages(packageName);
+
+        client.addToTab("Device", "Android Version", android.os.Build.VERSION.RELEASE);
+        client.addToTab("Device", "Device Type", android.os.Build.MODEL);
+
+        client.addToTab("Application", "Package Name", packageName);
+        client.addToTab("Application", "Package Version", packageVersion);
+
+        // Flush any queued exceptions
+        flushErrors();
+
+        logger.info("Bugsnag is loaded and ready to handle exceptions");
     }
 
     public static void notify(Throwable e) {
         notify(e, null);
     }
 
-    public static void notify(Throwable e, MetaData metaData) {
+    public static void notify(Throwable e, MetaData overrides) {
         if(client == null) {
             Log.e("Bugsnag", "You must call register with an apiKey before we can notify of exceptions!");
             return;
         }
 
-        client.notify(e, metaData);
+        // Generate diagnostic data
+        MetaData diagnostics = new Diagnostics(applicationContext);
+
+        // Merge local metaData into diagnostics
+        MetaData metaData = diagnostics.merge(overrides);
+
+        // Create the error object to send
+        final Error error = client.createError(e, metaData);
+
+        // Set the error's context
+        String topActivityName = ActivityStack.getTopActivityName();
+        if(topActivityName != null) {
+            error.setContext(topActivityName);
+        }
+
+        // Send the error
+        new AsyncTask <Void, Void, Void>() {
+            protected Void doInBackground(Void... voi) {
+                try {
+                    Notification notif = client.createNotification(error);
+                    notif.deliver();
+                } catch (IOException ex) {
+                    // Write error to disk for later sending
+                    logger.info("Could not send error(s) to Bugsnag, saving to disk to send later");
+                    writeErrorToDisk(error);
+                }
+
+                return null;
+            }
+        }.execute();
     }
 
     public static void setContext(String context) {
@@ -31,7 +111,8 @@ public class Bugsnag {
     }
 
     public static void setContext(Activity context) {
-        client.setContext(context);
+        String contextString = ActivityStack.getContextName(context);
+        client.setContext(contextString);
     }
 
     public static void setUserId(String userId) {
@@ -56,5 +137,131 @@ public class Bugsnag {
 
     public static void setEndpoint(String endpoint) {
         client.setEndpoint(endpoint);
+    }
+
+    private static void flushErrors() {
+        if(cachePath == null) return;
+
+        new AsyncTask <Void, Void, Void>() {
+            protected Void doInBackground(Void... voi) {
+                logger.debug("Flushing unsent errors (if any)");
+
+                // Create a notification
+                Notification notif = client.createNotification();
+                List<File> sentFiles = new LinkedList<File>();
+
+                // Look up all saved error files
+                File exceptionDir = new File(cachePath);
+                if(exceptionDir.exists() && exceptionDir.isDirectory()) {
+                    for(File errorFile : exceptionDir.listFiles()) {
+                        if(errorFile.exists() && errorFile.isFile()) {
+                            // Save filename in a "to delete" array
+                            sentFiles.add(errorFile);
+
+                            try {
+                                // Read error from disk and add to notification
+                                String errorString = FileUtils.readFileAsString(errorFile);
+                                notif.addError(errorString);
+
+                                logger.debug(String.format("Added unsent error (%s) to notification", errorFile.getName()));
+                            } catch (IOException e) {
+                                logger.warn("Problem reading unsent error from disk", e);
+                            }
+                        }
+                    }
+                }
+
+                try {
+                    // Send the notification
+                    notif.deliver();
+
+                    // Delete the files if notification worked
+                    for(File file : sentFiles) {
+                        logger.debug("Deleting unsent error file " + file.getName());
+                        file.delete();
+                    }
+                } catch (IOException e) {
+                    logger.info("Could not flush error(s) to Bugsnag, will try again later");
+                }
+
+                return null;
+            }
+        }.execute();
+    }
+
+    private static String getPackageName() {
+        return applicationContext.getPackageName();
+    }
+
+    private static String getPackageVersion(String packageName) {
+        String packageVersion = null;
+
+        try {
+            PackageInfo pi = applicationContext.getPackageManager().getPackageInfo(packageName, 0);
+            packageVersion = pi.versionName;
+        } catch(Exception e) {
+            logger.warn("Could not get package version", e);
+        }
+
+        return packageVersion;
+    }
+
+    private static String prepareCachePath() {
+        String path = null;
+
+        try {
+            path = applicationContext.getCacheDir().getAbsolutePath() + UNSENT_ERROR_PATH;
+
+            File outFile = new File(path);
+            outFile.mkdirs();
+            if(!outFile.exists()) {
+                logger.warn("Could not prepare cache directory");
+                path = null;
+            }
+        } catch(Exception e) {
+            logger.warn("Could not prepare cache directory", e);
+            path = null;
+        }
+
+        return path;
+    }
+
+    // TODO:JS Avoid StrictMode violations caused by getSharedPreferences
+    // TODO:JS Avoid StrictMode violations caused by UUID.randomUUID
+    private static String getUUID() {
+        final SharedPreferences settings = applicationContext.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        String uuid = settings.getString("userId", null);
+        if(uuid == null) {
+            uuid = UUID.randomUUID().toString();
+
+            // Save if for future
+            final String finalUuid = uuid;
+            new AsyncTask <Void, Void, Void>() {
+                protected Void doInBackground(Void... voi) {
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putString("userId", finalUuid);
+                    editor.commit();
+
+                    return null;
+                }
+            }.execute();
+        }
+        return uuid;
+    }
+
+    private static void writeErrorToDisk(Error error) {
+        if(cachePath == null) return;
+
+        String errorString = error.toString();
+        if(!errorString.isEmpty()) {
+            // Write the error to disk
+            String filename = String.format("%s%d.json", cachePath, System.currentTimeMillis());
+            try {
+                FileUtils.writeStringToFile(errorString, filename);
+                logger.debug(String.format("Saved unsent error to disk (%s) ", filename));
+            } catch (IOException e) {
+                logger.warn("Could not save error to disk", e);
+            }
+        }
     }
 }
