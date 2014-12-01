822fcf8cb2f28074c1cad9bafd38a0098470f803
James Smith <james@loopj.com>
2014-11-30 20:39:45 -0800

    Add more Error tests

--- src/androidTest/java/com/bugsnag/android/ErrorTest.java
+++ src/androidTest/java/com/bugsnag/android/ErrorTest.java
@@ -3,11 +3,8 @@ package com.bugsnag.android;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.bugsnag.android.Configuration;
-import com.bugsnag.android.Error;
-
 public class ErrorTest extends BugsnagTestCase {
-    public void testShouldIgnore() {
+    public void testShouldIgnoreClass() {
         Configuration config = new Configuration("api-key");
         config.ignoreClasses = new String[] {"java.io.IOException"};
 
@@ -34,12 +31,42 @@ public class ErrorTest extends BugsnagTestCase {
         assertEquals("Example message", error.getExceptionMessage());
     }
 
-    public void testToStream() throws JSONException {
+    public void testBasicSerialization() throws JSONException {
         Configuration config = new Configuration("api-key");
         Error error = new Error(config, new RuntimeException("Example message"));
 
         JSONObject errorJson = streamableToJson(error);
         assertEquals("warning", errorJson.get("severity"));
-        // TODO: Test all other fields are correct
+        assertEquals("2", errorJson.get("payloadVersion"));
+        assertNotNull(errorJson.get("severity"));
+        assertNotNull(errorJson.get("metaData"));
+        assertNotNull(errorJson.get("threads"));
+    }
+
+    public void testSetContext() throws JSONException {
+        Configuration config = new Configuration("api-key");
+        Error error = new Error(config, new RuntimeException("Example message"));
+        error.setContext("ExampleContext");
+
+        JSONObject errorJson = streamableToJson(error);
+        assertEquals("ExampleContext", errorJson.get("context"));
+    }
+
+    public void testSetGroupingHash() throws JSONException {
+        Configuration config = new Configuration("api-key");
+        Error error = new Error(config, new RuntimeException("Example message"));
+        error.setGroupingHash("herpderp");
+
+        JSONObject errorJson = streamableToJson(error);
+        assertEquals("herpderp", errorJson.get("groupingHash"));
+    }
+
+    public void testSetSeverity() throws JSONException {
+        Configuration config = new Configuration("api-key");
+        Error error = new Error(config, new RuntimeException("Example message"));
+        error.setSeverity(Severity.INFO);
+
+        JSONObject errorJson = streamableToJson(error);
+        assertEquals("info", errorJson.get("severity"));
     }
 }
