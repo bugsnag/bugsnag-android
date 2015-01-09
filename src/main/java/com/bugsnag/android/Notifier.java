e9dcf483d59ea3aedb67c32f8fada8b6725a63fb
James Smith <james@loopj.com>
2015-01-08 23:48:59 -0800

    v3.2.2

--- src/main/java/com/bugsnag/android/Notifier.java
+++ src/main/java/com/bugsnag/android/Notifier.java
@@ -7,7 +7,7 @@ import java.io.IOException;
  */
 class Notifier implements JsonStream.Streamable {
     static final String NOTIFIER_NAME = "Android Bugsnag Notifier";
-    static final String NOTIFIER_VERSION = "3.2.1";
+    static final String NOTIFIER_VERSION = "3.2.2";
     static final String NOTIFIER_URL = "https://bugsnag.com";
 
     private static Notifier instance = new Notifier();
