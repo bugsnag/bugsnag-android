c7e540eb74b89c4e67fb553270d746de0b93b6d5
darken <darken@darken.eu>
2015-12-28 23:43:12 +0100

    A few more tests, just for fun :).

--- src/androidTest/java/com/bugsnag/android/DeviceDataTest.java
+++ src/androidTest/java/com/bugsnag/android/DeviceDataTest.java
@@ -1,17 +1,22 @@
 package com.bugsnag.android;
 
-import java.io.IOException;
+import android.util.DisplayMetrics;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.util.DisplayMetrics;
+import java.io.IOException;
 
 public class DeviceDataTest extends BugsnagTestCase {
     public void testSaneValues() throws JSONException, IOException {
         Configuration config = new Configuration("some-api-key");
         DeviceData deviceData = new DeviceData(getContext());
         JSONObject deviceDataJson = streamableToJson(deviceData);
+
+        assertEquals("android", deviceDataJson.getString("osName"));
+        assertTrue(deviceDataJson.getString("manufacturer").length() > 1);
+        assertTrue(deviceDataJson.getString("brand").length() > 1);
+        assertTrue(deviceDataJson.getString("model").length() > 1);
 
         assertTrue(deviceDataJson.getDouble("screenDensity") > 0);
         assertTrue(deviceDataJson.getDouble("dpi") >= DisplayMetrics.DENSITY_LOW);
@@ -20,7 +25,7 @@ public class DeviceDataTest extends BugsnagTestCase {
         assertNotNull(deviceDataJson.getBoolean("jailbroken"));
         assertNotNull(deviceDataJson.getString("locale"));
 
-        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.FROYO) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.FROYO) {
             // Emulators returned null for android id before android 2.2
             assertNotNull(deviceDataJson.getString("id"));
         }
