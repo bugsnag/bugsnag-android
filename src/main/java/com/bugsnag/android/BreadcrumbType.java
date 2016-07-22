9f2108584b35dc8e250c428a5ff1c537863e653b
Delisa Mason <iskanamagus@gmail.com>
2016-07-21 19:00:51 -0700

    Document breadcrumb types

--- src/main/java/com/bugsnag/android/BreadcrumbType.java
+++ src/main/java/com/bugsnag/android/BreadcrumbType.java
@@ -1,13 +1,40 @@
 package com.bugsnag.android;
 
+/**
+ * Recognized types of breadcrumbs
+ */
 public enum BreadcrumbType {
+    /**
+     * An error was sent to Bugsnag (internal use only)
+     */
     ERROR ("error"),
+    /**
+     * A log message
+     */
     LOG ("log"),
+    /**
+     * A manual invocation of `leaveBreadcrumb` (default)
+     */
     MANUAL ("manual"),
+    /**
+     * A navigation event, such as a window opening or closing
+     */
     NAVIGATION ("navigation"),
+    /**
+     * A background process such as a database query
+     */
     PROCESS ("process"),
+    /**
+     * A network request
+     */
     REQUEST ("request"),
+    /**
+     * A change in application state, such as launch or memory warning
+     */
     STATE ("state"),
+    /**
+     * A user action, such as tapping a button
+     */
     USER ("user");
 
     private final String type;
