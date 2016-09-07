e34d13b31ffc2e1bc107d3dc88bcf81c7b5721b0
Dave Perryman <dave@bugsnag.com>
2016-09-07 14:38:34 +0100

    Remove duplicated fields (#118)
    Add new BugsnagException class to remove duplicate fields from the 
    error object

---
+++ src/main/java/com/bugsnag/android/BugsnagException.java
@@ -0,0 +1,30 @@
+package com.bugsnag.android;
+
+/**
+ * Used to store information about an exception that was not provided with an exception object
+ */
+public class BugsnagException extends Throwable {
+
+    /** The name of the exception (used instead of the exception class) */
+    private String name;
+
+    /**
+     * Constructor
+     * @param name The name of the exception (used instead of the exception class)
+     * @param message The exception message
+     * @param frames The exception stack trace
+     */
+    public BugsnagException(String name, String message, StackTraceElement[] frames) {
+        super(message);
+
+        super.setStackTrace(frames);
+        this.name = name;
+    }
+
+    /**
+     * @return The name of the exception (used instead of the exception class)
+     */
+    public String getName() {
+        return name;
+    }
+}
