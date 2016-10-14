6e8673751689c9ba7e141d11755e0444b84afaa4
Dave Perryman <dave@bugsnag.com>
2016-10-14 11:19:27 +0100

    Changed methods that should have been static

--- src/main/java/com/bugsnag/android/Bugsnag.java
+++ src/main/java/com/bugsnag/android/Bugsnag.java
@@ -75,7 +75,7 @@ public final class Bugsnag {
      * Gets the context to be sent to Bugsnag.
      * @return Context
      */
-    public String getContext() {
+    public static String getContext() {
         return getClient().getContext();
     }
 
@@ -218,7 +218,7 @@ public final class Bugsnag {
     /**
      * Removes the current user data and sets it back to defaults
      */
-    public void clearUser() {
+    public static void clearUser() {
         getClient().clearUser();
     }
 
