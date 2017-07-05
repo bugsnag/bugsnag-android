f06d4732d26ec633544f2a8d6a06c450d139a411
Jamie Lynch <fractalwrench@gmail.com>
2017-07-05 11:29:21 +0100

    add error store test to check files written to disk

--- src/androidTest/java/com/bugsnag/android/ClientRejectedExecutionTest.java
+++ src/androidTest/java/com/bugsnag/android/ClientRejectedExecutionTest.java
@@ -1,15 +1,12 @@
 package com.bugsnag.android;
 
-import org.json.JSONObject;
 
 import java.io.File;
-import java.io.StringWriter;
 
 public class ClientRejectedExecutionTest extends BugsnagTestCase {
 
     private static final int MAX_ALLOWED_TASKS = 128;
     private static final int TASK_COUNT = MAX_ALLOWED_TASKS * 2;
-    public static final String ERROR_MESSAGE = "Rejected Execution";
 
     private Client client;
     private File errorStorageDir;
@@ -21,18 +18,7 @@ public class ClientRejectedExecutionTest extends BugsnagTestCase {
         client = new Client(getContext(), "api-key");
         ErrorStore errorStore = client.errorStore;
         errorStorageDir = new File(errorStore.path);
-        clearFilesInDir(errorStorageDir);
-    }
-
-    private void clearFilesInDir(File storageDir) {
-        if (!storageDir.isDirectory()) {
-            throw new IllegalArgumentException();
-        }
-        for (File file : storageDir.listFiles()) {
-            if (!file.isDirectory()) {
-                file.delete();
-            }
-        }
+        FileUtils.clearFilesInDir(errorStorageDir);
     }
 
     /**
@@ -43,26 +29,14 @@ public class ClientRejectedExecutionTest extends BugsnagTestCase {
         assertEquals(0, errorStorageDir.listFiles().length);
 
         for (int k = 0; k < TASK_COUNT; k++) {
-            client.notify(new Throwable(ERROR_MESSAGE));
+            client.notify(new Throwable("Rejected Execution"));
         }
-        checkFirstErrorReportFile();
-    }
 
-    private void checkFirstErrorReportFile() throws Exception {
         // the exact number of files is indeterminate (dependent on the ThreadPoolExecutor)
         File[] files = errorStorageDir.listFiles();
         assertTrue(files.length > 0);
-
-        // ensure that the file isn't empty
         File errorFile = files[0];
-        assertFalse(errorFile.length() <= 0);
-
-        // ensure the file can be serialised into JSON report
-        Report report = new Report("abc", errorFile);
-        StringWriter stringWriter = new StringWriter();
-        report.toStream(new JsonStream(stringWriter));
-        JSONObject jsonObject = new JSONObject(stringWriter.toString());
-        assertNotNull(jsonObject);
+        ErrorStoreTest.checkFirstErrorReportFile(errorFile);
     }
 
 }
