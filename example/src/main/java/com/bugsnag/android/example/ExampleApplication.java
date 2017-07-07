d2e48f30ca41534f1731844ddb5ab13e956f9411
Jamie Lynch <fractalwrench@gmail.com>
2017-07-07 10:02:38 +0100

    setup automatic breadcrumbs for activity lifecycle callbacks on API 14+

--- example/src/main/java/com/bugsnag/android/example/ExampleApplication.java
+++ example/src/main/java/com/bugsnag/android/example/ExampleApplication.java
@@ -1,6 +1,8 @@
 package com.bugsnag.android.example;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 
 import com.bugsnag.android.Bugsnag;
 
@@ -12,6 +14,44 @@ public class ExampleApplication extends Application {
 
         // Initialize the Bugsnag client
         Bugsnag.init(this);
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+
     }
 
 }
