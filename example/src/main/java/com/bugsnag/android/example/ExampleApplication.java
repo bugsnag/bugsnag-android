f92d00e7bec092c009d076407a4caea16ea17a28
Jamie Lynch <fractalwrench@gmail.com>
2017-07-07 10:12:23 +0100

    remove unused activity lifecycle callback from example code

--- example/src/main/java/com/bugsnag/android/example/ExampleApplication.java
+++ example/src/main/java/com/bugsnag/android/example/ExampleApplication.java
@@ -1,8 +1,6 @@
 package com.bugsnag.android.example;
 
-import android.app.Activity;
 import android.app.Application;
-import android.os.Bundle;
 
 import com.bugsnag.android.Bugsnag;
 
@@ -14,44 +12,6 @@ public class ExampleApplication extends Application {
 
         // Initialize the Bugsnag client
         Bugsnag.init(this);
-
-        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
-            @Override
-            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-
-            }
-
-            @Override
-            public void onActivityStarted(Activity activity) {
-
-            }
-
-            @Override
-            public void onActivityResumed(Activity activity) {
-
-            }
-
-            @Override
-            public void onActivityPaused(Activity activity) {
-
-            }
-
-            @Override
-            public void onActivityStopped(Activity activity) {
-
-            }
-
-            @Override
-            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-
-            }
-
-            @Override
-            public void onActivityDestroyed(Activity activity) {
-
-            }
-        });
-
     }
 
 }
