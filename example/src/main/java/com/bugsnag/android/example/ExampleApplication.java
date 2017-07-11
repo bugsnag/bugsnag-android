687f5846928d2a8a6005c7229472238cd2954fcc
Jamie Lynch <fractalwrench@gmail.com>
2017-07-11 16:37:50 +0100

    remove unused code

--- example/src/main/java/com/bugsnag/android/example/ExampleApplication.java
+++ example/src/main/java/com/bugsnag/android/example/ExampleApplication.java
@@ -1,8 +1,6 @@
 package com.bugsnag.android.example;
 
 import android.app.Application;
-import android.os.Build;
-import android.os.StrictMode;
 
 import com.bugsnag.android.Bugsnag;
 
@@ -14,19 +12,6 @@ public class ExampleApplication extends Application {
 
         // Initialize the Bugsnag client
         Bugsnag.init(this);
-
-        if (false && Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
-            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-                .detectAll()
-                .penaltyDeath()
-                .build());
-
-            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
-                .detectAll()
-                .penaltyDeath()
-                .build());
-        }
-
     }
 
 }
