ad416313fdde1319a36de33af14e2c79d3c73423
Jamie Lynch <fractalwrench@gmail.com>
2017-07-11 16:32:40 +0100

    fix crash when rotating screen (handle when event receiver not registered)

--- example/src/main/java/com/bugsnag/android/example/ExampleApplication.java
+++ example/src/main/java/com/bugsnag/android/example/ExampleApplication.java
@@ -1,6 +1,8 @@
 package com.bugsnag.android.example;
 
 import android.app.Application;
+import android.os.Build;
+import android.os.StrictMode;
 
 import com.bugsnag.android.Bugsnag;
 
@@ -12,6 +14,19 @@ public class ExampleApplication extends Application {
 
         // Initialize the Bugsnag client
         Bugsnag.init(this);
+
+        if (false && Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyDeath()
+                .build());
+
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .penaltyDeath()
+                .build());
+        }
+
     }
 
 }
