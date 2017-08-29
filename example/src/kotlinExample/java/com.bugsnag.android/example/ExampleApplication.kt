a8476dbbb7626e6f8b92365f6f22fc7573cfd1bc
Jamie Lynch <fractalwrench@gmail.com>
2017-08-29 10:07:23 +0100

    update example code

--- example/src/kotlinExample/java/com.bugsnag.android/example/ExampleApplication.kt
+++ example/src/kotlinExample/java/com.bugsnag.android/example/ExampleApplication.kt
@@ -3,7 +3,6 @@ package com.bugsnag.android.example
 import android.app.Application
 
 import com.bugsnag.android.Bugsnag
-import com.bugsnag.android.Configuration
 
 class ExampleApplication : Application() {
 
@@ -11,10 +10,7 @@ class ExampleApplication : Application() {
         super.onCreate()
 
         // Initialize the Bugsnag client
-        val configuration = Configuration("f35a2472bd230ac0ab0f52715bbdc65d")
-        configuration.launchCrashThresholdMs = 20000
-        Bugsnag.init(this, configuration)
-
+        Bugsnag.init(this)
     }
 
 }
