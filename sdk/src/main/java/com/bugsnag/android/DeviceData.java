97af431f0ba3c400049d33b93a2a9a5037275a16
fractalwrench <fractalwrench@gmail.com>
2017-10-27 16:05:26 +0100

    allow setting device id to null

--- sdk/src/main/java/com/bugsnag/android/DeviceData.java
+++ sdk/src/main/java/com/bugsnag/android/DeviceData.java
@@ -38,7 +38,7 @@ class DeviceData implements JsonStream.Streamable {
     @NonNull
     protected final String locale;
     @Nullable
-    protected final String id;
+    protected String id;
     @NonNull
     protected final String[] cpuAbi;
 
@@ -85,6 +85,10 @@ class DeviceData implements JsonStream.Streamable {
     @NonNull
     public String getUserId() {
         return id;
+    }
+
+    void setId(@Nullable String id) {
+        this.id = id;
     }
 
     /**
