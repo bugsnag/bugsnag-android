e8161b9d22112bae356e2d362e2098fd0e0550d2
fractalwrench <fractalwrench@gmail.com>
2017-11-01 12:33:08 +0000

    add sessiontracking api client

--- sdk/src/main/java/com/bugsnag/android/DefaultHttpClient.java
+++ sdk/src/main/java/com/bugsnag/android/DefaultHttpClient.java
@@ -10,7 +10,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
 
-class DefaultHttpClient implements ErrorReportApiClient {
+class DefaultHttpClient implements ErrorReportApiClient, SessionTrackingApiClient {
 
     private final ConnectivityManager connectivityManager;
 
@@ -19,13 +19,33 @@ class DefaultHttpClient implements ErrorReportApiClient {
     }
 
     @Override
-    public void postReport(String urlString, Report report, Map<String, String> headers) throws NetworkException, BadResponseException {
+    public void postReport(String urlString,
+                           Report report,
+                           Map<String, String> headers) throws NetworkException, BadResponseException {
+        int status = makeRequest(urlString, report, headers);
 
-        if (!hasNetworkConnection()) { // conserve device battery by avoiding radio use
-            throw new NetworkException(urlString, new RuntimeException("No network connection available"));
+        if (status / 100 != 2) {
+            throw new BadResponseException(urlString, status);
         }
+    }
 
+    @Override
+    public void postSessionTrackingPayload(String urlString,
+                                           SessionTrackingPayload payload,
+                                           Map<String, String> headers) throws NetworkException, BadResponseException {
+        int status = makeRequest(urlString, payload, headers);
+
+        if (status != 202) {
+            throw new BadResponseException(urlString, status);
+        }
+    }
+
+    private int makeRequest(String urlString,
+                            JsonStream.Streamable streamable,
+                            Map<String, String> headers) throws NetworkException {
+        checkHasNetworkConnection(urlString);
         HttpURLConnection conn = null;
+
         try {
             URL url = new URL(urlString);
             conn = (HttpURLConnection) url.openConnection();
@@ -38,21 +58,19 @@ class DefaultHttpClient implements ErrorReportApiClient {
             }
 
             OutputStream out = null;
+
             try {
                 out = conn.getOutputStream();
-
                 JsonStream stream = new JsonStream(new OutputStreamWriter(out));
-                report.toStream(stream);
+                streamable.toStream(stream);
                 stream.close();
             } finally {
                 IOUtils.closeQuietly(out);
             }
 
+
             // End the request, get the response code
-            int status = conn.getResponseCode();
-            if (status / 100 != 2) {
-                throw new BadResponseException(urlString, status);
-            }
+            return conn.getResponseCode();
         } catch (IOException e) {
             throw new NetworkException(urlString, e);
         } finally {
@@ -60,9 +78,12 @@ class DefaultHttpClient implements ErrorReportApiClient {
         }
     }
 
-    private boolean hasNetworkConnection() {
+    private void checkHasNetworkConnection(String urlString) throws NetworkException {
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+
+        if (!(activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting())) { // conserve device battery by avoiding radio use
+            throw new NetworkException(urlString, new RuntimeException("No network connection available"));
+        }
     }
 
 }
---
+++ sdk/src/main/java/com/bugsnag/android/SessionTrackingPayload.java
@@ -0,0 +1,14 @@
+package com.bugsnag.android;
+
+import android.support.annotation.NonNull;
+
+import java.io.IOException;
+
+public class SessionTrackingPayload implements JsonStream.Streamable {
+
+    @Override
+    public void toStream(@NonNull JsonStream stream) throws IOException {
+        // TODO
+    }
+
+}
