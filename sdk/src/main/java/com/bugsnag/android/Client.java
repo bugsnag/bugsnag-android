6dd2322a75de4ddbd824803af55f75bdf270ad4c
fractalwrench <fractalwrench@gmail.com>
2017-11-13 14:36:42 +0000

    begin integration testing

--- sdk/src/main/java/com/bugsnag/android/Client.java
+++ sdk/src/main/java/com/bugsnag/android/Client.java
@@ -42,7 +42,7 @@ enum DeliveryStyle {
  */
 public class Client extends Observable implements Observer {
 
-    private static final long SESSION_LOOP_MS = 5 * 1000; // TODO change to X mins
+    private static final long SESSION_LOOP_MS = 30 * 1000; // TODO change to X mins
     private static final boolean BLOCKING = true;
     private static final String SHARED_PREF_KEY = "com.bugsnag.android";
     private static final String BUGSNAG_NAMESPACE = "com.bugsnag.android";
