c42b67535b5ffc60ed3db351a77c8f448cbdd6a5
fractalwrench <fractalwrench@gmail.com>
2017-11-22 09:37:27 +0000

    add test for lifecycle logging

--- sdk/src/main/java/com/bugsnag/android/LifecycleBreadcrumbLogger.java
+++ sdk/src/main/java/com/bugsnag/android/LifecycleBreadcrumbLogger.java
@@ -15,7 +15,7 @@ class LifecycleBreadcrumbLogger implements Application.ActivityLifecycleCallback
 
     private static final String KEY_LIFECYCLE_CALLBACK = "ActivityLifecycle";
 
-    private final Queue<Pair<String, String>> queue = new ConcurrentLinkedQueue<>();
+    final Queue<Pair<String, String>> queue = new ConcurrentLinkedQueue<>();
     private final Client client;
 
     LifecycleBreadcrumbLogger(Client client) {
@@ -24,42 +24,44 @@ class LifecycleBreadcrumbLogger implements Application.ActivityLifecycleCallback
 
     @Override
     public void onActivityCreated(@NonNull Activity activity, Bundle savedInstanceState) {
-        leaveLifecycleBreadcrumb(activity, "onCreate()");
+        leaveLifecycleBreadcrumb(getActivityName(activity), "onCreate()");
     }
 
     @Override
     public void onActivityStarted(@NonNull Activity activity) {
-        leaveLifecycleBreadcrumb(activity, "onStart()");
+        leaveLifecycleBreadcrumb(getActivityName(activity), "onStart()");
     }
 
     @Override
     public void onActivityResumed(@NonNull Activity activity) {
-        leaveLifecycleBreadcrumb(activity, "onResume()");
+        leaveLifecycleBreadcrumb(getActivityName(activity), "onResume()");
     }
 
     @Override
     public void onActivityPaused(@NonNull Activity activity) {
-        leaveLifecycleBreadcrumb(activity, "onPause()");
+        leaveLifecycleBreadcrumb(getActivityName(activity), "onPause()");
     }
 
     @Override
     public void onActivityStopped(@NonNull Activity activity) {
-        leaveLifecycleBreadcrumb(activity, "onStop()");
+        leaveLifecycleBreadcrumb(getActivityName(activity), "onStop()");
     }
 
     @Override
     public void onActivitySaveInstanceState(@NonNull Activity activity, Bundle outState) {
-        leaveLifecycleBreadcrumb(activity, "onSaveInstanceState()");
+        leaveLifecycleBreadcrumb(getActivityName(activity), "onSaveInstanceState()");
     }
 
     @Override
     public void onActivityDestroyed(@NonNull Activity activity) {
-        leaveLifecycleBreadcrumb(activity, "onDestroy()");
+        leaveLifecycleBreadcrumb(getActivityName(activity), "onDestroy()");
     }
 
-    private void leaveLifecycleBreadcrumb(@NonNull Activity activity, String lifecycleCallback) {
-        String activityName = activity.getClass().getSimpleName();
+    private String getActivityName(@NonNull Activity activity) {
+        return activity.getClass().getSimpleName();
+    }
 
+    void leaveLifecycleBreadcrumb(String activityName, String lifecycleCallback) {
         if (client == null) { // not initialised yet, enqueue breadcrumbs for later
             queue.add(new Pair<>(activityName, lifecycleCallback));
         } else {
