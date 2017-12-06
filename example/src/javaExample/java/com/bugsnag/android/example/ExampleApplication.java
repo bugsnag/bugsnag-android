30eb0b8559379609ee5b479fdbb8116bb25d2b0c
fractalwrench <fractalwrench@gmail.com>
2017-12-06 13:11:27 +0000

    respect auto capture config

--- example/src/javaExample/java/com/bugsnag/android/example/ExampleApplication.java
+++ example/src/javaExample/java/com/bugsnag/android/example/ExampleApplication.java
@@ -3,22 +3,13 @@ package com.bugsnag.android.example;
 import android.app.Application;
 
 import com.bugsnag.android.Bugsnag;
-import com.bugsnag.android.Configuration;
 
 public class ExampleApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-//        Bugsnag.init(this);
-
-        Configuration config = new Configuration("9f2996871fb381de73bfb0bea455c28b");
-        config.setSessionEndpoint("http://10.0.2.2:10000");
-        config.setEndpoint("http://10.0.2.2:8000");
-
-//        config.setAutoCaptureSessions(true);
-        Bugsnag.init(this, config);
-        Bugsnag.startSession();
+        Bugsnag.init(this);
 
     }
 
