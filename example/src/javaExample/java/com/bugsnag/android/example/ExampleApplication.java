a31f854b3386787eea05186ca3f0f5cdc71fd704
fractalwrench <fractalwrench@gmail.com>
2018-01-23 11:01:44 +0000

    reset application init to default

--- example/src/javaExample/java/com/bugsnag/android/example/ExampleApplication.java
+++ example/src/javaExample/java/com/bugsnag/android/example/ExampleApplication.java
@@ -3,20 +3,13 @@ package com.bugsnag.android.example;
 import android.app.Application;
 
 import com.bugsnag.android.Bugsnag;
-import com.bugsnag.android.Configuration;
 
 public class ExampleApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Configuration config = new Configuration("f35a2472bd230ac0ab0f52715bbdc65d");
-        config.setSessionEndpoint("http://10.0.2.2:8092");
-        config.setEndpoint("http://10.0.2.2:8092");
-
-        Bugsnag.init(this, config);
-        Bugsnag.setAutoCaptureSessions(true);
-
+        Bugsnag.init(this);
     }
 
 }
