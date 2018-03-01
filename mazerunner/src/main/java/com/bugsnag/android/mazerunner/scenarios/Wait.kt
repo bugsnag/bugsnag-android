a2f05f81a0609d117f6de036ab4604bf79395d2f
fractalwrench <fractalwrench@gmail.com>
2018-03-01 13:43:59 +0000

    add cache scenario for report

---
+++ mazerunner/src/main/java/com/bugsnag/android/mazerunner/scenarios/Wait.kt
@@ -0,0 +1,12 @@
+package com.bugsnag.android.mazerunner.scenarios
+
+import android.content.Context
+import com.bugsnag.android.Configuration
+
+internal class Wait(config: Configuration, context: Context) : Scenario(config, context) {
+
+    override fun run() {
+        super.run()
+    }
+
+}
