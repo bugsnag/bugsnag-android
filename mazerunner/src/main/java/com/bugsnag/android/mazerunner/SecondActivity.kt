c503f90257814dc1f4e7fbbb353719eea3de5031
fractalwrench <fractalwrench@gmail.com>
2018-03-01 11:34:55 +0000

    add manual/auto context scenarios

---
+++ mazerunner/src/main/java/com/bugsnag/android/mazerunner/SecondActivity.kt
@@ -0,0 +1,13 @@
+package com.bugsnag.android.mazerunner
+
+import android.app.Activity
+import android.os.Bundle
+
+class SecondActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
