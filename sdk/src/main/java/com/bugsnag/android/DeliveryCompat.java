1582bd071c3aa360dbe1bbe49a0909922b801adf
fractalwrench <fractalwrench@gmail.com>
2018-05-08 15:42:47 +0100

    docs: document deliverycompat class

--- sdk/src/main/java/com/bugsnag/android/DeliveryCompat.java
+++ sdk/src/main/java/com/bugsnag/android/DeliveryCompat.java
@@ -3,6 +3,11 @@ package com.bugsnag.android;
 import static com.bugsnag.android.DeliveryFailureException.Reason.CONNECTIVITY;
 import static com.bugsnag.android.DeliveryFailureException.Reason.REQUEST_FAILURE;
 
+/**
+ * A compatibility implementation of {@link Delivery} which wraps {@link ErrorReportApiClient} and
+ * {@link SessionTrackingApiClient}. This class allows for backwards compatibility for users still
+ * utilising the old API, and should be removed in the next major version.
+ */
 class DeliveryCompat implements Delivery {
 
     final Delivery delivery;
