0c110145af7351bd934104f1b5b5113f331d5513
fractalwrench <fractalwrench@gmail.com>
2018-05-10 12:03:07 +0100

    test: add test to ensure that jsonwriter closes underlying stream

---
+++ sdk/src/androidTest/java/com/bugsnag/android/JsonWriterTest.java
@@ -0,0 +1,27 @@
+package com.bugsnag.android;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import static org.junit.Assert.*;
+
+public class JsonWriterTest {
+
+    @Test(expected = IOException.class)
+    public void testClose() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        OutputStreamWriter writer = new OutputStreamWriter(baos);
+        JsonWriter jsonWriter = new JsonWriter(writer);
+
+        jsonWriter.beginObject().endObject();
+        jsonWriter.flush();
+        assertEquals("{}", new String(baos.toByteArray(), "UTF-8"));
+
+        jsonWriter.close();
+        writer.write(5); // can't write to a closed stream, throws IOException
+    }
+
+}
