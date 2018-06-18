c9840ee625f107fcb75bdb23f731c49c22ff4592
fractalwrench <fractalwrench@gmail.com>
2018-06-18 16:38:37 +0100

    docs: add javadoc to appdata classes for accessors

--- sdk/src/main/java/com/bugsnag/android/AppData.java
+++ sdk/src/main/java/com/bugsnag/android/AppData.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 /**
  * Information about the running Android app, including app name, version and release stage.
  */
-class AppData extends AppDataSummary {
+public class AppData extends AppDataSummary {
 
     private static final long startTimeMs = SystemClock.elapsedRealtime();
 
@@ -63,7 +63,7 @@ class AppData extends AppDataSummary {
         // TODO migrate legacy fields
         writer.name("name").value(appName);
         writer.name("packageName").value(packageName);
-        writer.name("versionName").value(versionName);
+        writer.name("versionName").value(getVersionName());
         writer.name("activeScreen").value(getActiveScreenClass());
         writer.name("memoryUsage").value(getMemoryUsage());
         writer.name("lowMemory").value(isLowMemory(appContext));
@@ -88,45 +88,84 @@ class AppData extends AppDataSummary {
         return null;
     }
 
-
+    /**
+     * @return the application's package name
+     */
     @NonNull
     public String getPackageName() {
         return packageName;
     }
 
+    /**
+     * Overrides the application's default package name
+     *
+     * @param packageName the package name
+     */
     public void setPackageName(@NonNull String packageName) {
         this.packageName = packageName;
     }
 
+    /**
+     * @return the application's bugsnag build UUID
+     */
     @Nullable
     public String getBuildUuid() {
         return buildUuid;
     }
 
+    /**
+     * Overrides the application's default bugsnag build UUID
+     *
+     * @param buildUuid the bugsnag build UUID
+     */
     public void setBuildUuid(@Nullable String buildUuid) {
         this.buildUuid = buildUuid;
     }
 
+    /**
+     * @return the duration in ms for which the app has been running
+     */
     public long getDuration() {
         return durationMs;
     }
 
+    /**
+     * Overrides the duration in ms for which the app has been running
+     *
+     * @param durationMs the new duration in ms
+     */
     public void setDuration(long durationMs) {
         this.durationMs = durationMs;
     }
 
+    /**
+     * @return the duration in ms for which the app has been running in the foreground
+     */
     public long getDurationInForeground() {
         return foregroundMs;
     }
 
+    /**
+     * Overrides the duration in ms for which the app has been running in the foreground
+     *
+     * @param foregroundMs the new duration in ms
+     */
     public void setDurationInForeground(long foregroundMs) {
         this.foregroundMs = foregroundMs;
     }
 
+    /**
+     * @return whether the app is in the foreground or not
+     */
     public boolean isInForeground() {
         return inForeground;
     }
 
+    /**
+     * Overrides whether the app is in the foreground or not
+     *
+     * @param inForeground whether the app is in the foreground
+     */
     public void setInForeground(boolean inForeground) {
         this.inForeground = inForeground;
     }
--- sdk/src/main/java/com/bugsnag/android/AppDataSummary.java
+++ sdk/src/main/java/com/bugsnag/android/AppDataSummary.java
@@ -15,16 +15,16 @@ import java.io.IOException;
  * App information in this class is cached during construction for faster
  * subsequent lookups and to reduce GC overhead.
  */
-class AppDataSummary implements JsonStream.Streamable {
+public class AppDataSummary implements JsonStream.Streamable {
 
     static final String RELEASE_STAGE_DEVELOPMENT = "development";
     static final String RELEASE_STAGE_PRODUCTION = "production";
 
     @Nullable
-    protected Integer versionCode;
+    private Integer versionCode;
 
     @Nullable
-    protected String versionName;
+    private String versionName;
 
     @NonNull
     private String releaseStage;
@@ -74,47 +74,81 @@ class AppDataSummary implements JsonStream.Streamable {
             .name("codeBundleId").value(codeBundleId);
     }
 
+    /**
+     * @return the application's version code
+     */
     @Nullable
     public Integer getVersionCode() {
         return versionCode;
     }
 
+    /**
+     * Overrides the application's default version code
+     *
+     * @param versionCode the version code
+     */
     public void setVersionCode(@Nullable Integer versionCode) {
         this.versionCode = versionCode;
     }
 
+    /**
+     * @return the application's version name
+     */
     @Nullable
     public String getVersionName() {
         return versionName;
     }
 
+    /**
+     * Overrides the application's default version name
+     *
+     * @param versionName the version name
+     */
     public void setVersionName(@Nullable String versionName) {
         this.versionName = versionName;
     }
 
+    /**
+     * @return the application's release stage
+     */
     @NonNull
     public String getReleaseStage() {
         return releaseStage;
     }
 
+    /**
+     * Overrides the application's default release stage
+     *
+     * @param releaseStage the release stage
+     */
     public void setReleaseStage(@NonNull String releaseStage) {
         this.releaseStage = releaseStage;
     }
 
     @NonNull
+    @InternalApi
     public String getNotifierType() {
         return notifierType;
     }
 
+    @InternalApi
     public void setNotifierType(@NonNull String notifierType) {
         this.notifierType = notifierType;
     }
 
+    /**
+     * @return the application's code bundle ID, if it exists
+     */
     @Nullable
     public String getCodeBundleId() {
         return codeBundleId;
     }
 
+    /**
+     * Overrides the application's default code bundle ID
+     *
+     * @param codeBundleId the code bundle ID
+     */
     public void setCodeBundleId(@Nullable String codeBundleId) {
         this.codeBundleId = codeBundleId;
     }
