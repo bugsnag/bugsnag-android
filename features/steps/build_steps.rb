73ce339f32e24b3b4c0cd9c4d2b664394af70cb4
Delisa Mason <iskanamagus@gmail.com>
2018-08-22 12:20:20 -0700

    tests(maze): Move test fixtures from root into fixtures dir
    Replace source dependency on bugsnag-android with aar dependency.
    Note: bugsnag-android also requires infer to be installed.

--- features/steps/build_steps.rb
+++ features/steps/build_steps.rb
@@ -1,7 +1,7 @@
 When("I run {string} with the defaults") do |eventType|
   steps %Q{
     When I start Android emulator "newnexus"
-    And I install the "com.bugsnag.android.mazerunner" Android app from "mazerunner/build/outputs/apk/release/mazerunner-release.apk"
+    And I install the "com.bugsnag.android.mazerunner" Android app from "features/fixtures/mazerunner/build/outputs/apk/release/mazerunner-release.apk"
     And I clear the "com.bugsnag.android.mazerunner" Android app data
     And I set environment variable "BUGSNAG_API_KEY" to "a35a2a72bd230ac0aa0f52715bbdc6aa"
     And I set environment variable "EVENT_TYPE" to "#{eventType}"
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,13 @@
 # Configure app environment
 
 run_required_commands([
-  ["./gradlew", "clean", "mazerunner:assembleRelease"],
+  ["./gradlew", "clean", "assembleRelease"],
+  ["cp", "sdk/build/outputs/aar/bugsnag-android-release.aar",
+   "features/fixtures/mazerunner/libs/bugsnag-android.aar"],
 ])
+
+Dir.chdir('features/fixtures/mazerunner') do
+  run_required_commands([
+    ["../../../gradlew", "clean", "assembleRelease"],
+  ])
+end
