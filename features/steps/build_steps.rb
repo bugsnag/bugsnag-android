27ee302ad50ac882f1b7207be6d8babef86bf2b5
Alex Moinet <alex@bugsnag.com>
2018-09-05 15:18:04 +0100

    test(maze): Add missing step definitions

--- features/steps/build_steps.rb
+++ features/steps/build_steps.rb
@@ -1,5 +1,20 @@
 When("I run {string} with the defaults") do |event_type|
-  When("I run #{event_type} against @newnexus")
+  step("I run #{event_type} against newnexus")
+end
+
+When(/^I run "([^"]+)"$/) do |event_type|
+  step("I run \"#{event_type}\" against \"#{ENV['ANDROID_EMULATOR']}\"")
+end
+
+When(/^I run "([^"]+)" against "([^"]+)"$/) do |event_type, emulator|
+  steps %Q{
+    When I start emulator "#{emulator}"
+    And I install the "com.bugsnag.android.mazerunner" Android app from "features/fixtures/mazerunner/build/outputs/apk/release/mazerunner-release.apk"
+    And I clear the "com.bugsnag.android.mazerunner" Android app data
+    And I set environment variable "BUGSNAG_API_KEY" to "a35a2a72bd230ac0aa0f52715bbdc6aa"
+    And I set environment variable "EVENT_TYPE" to "#{event_type}"
+    And I start the "com.bugsnag.android.mazerunner" Android app using the "com.bugsnag.android.mazerunner.MainActivity" activity
+  }
 end
 
 When("I start emulator {string}") do |emulator|
@@ -7,17 +22,6 @@ When("I start emulator {string}") do |emulator|
     When I set environment variable "ANDROID_EMULATOR" to "#{emulator}"
     And I run the script "features/scripts/launch_emulator.sh"
     And I run the script "await-android-emulator.sh" synchronously
-  }
-end
-
-When("I run {string}") do |event_type|
-  steps %Q{
-    When I start emulator "#{ENV['ANDROID_EMULATOR']}"
-    And I install the "com.bugsnag.android.mazerunner" Android app from "features/fixtures/mazerunner/build/outputs/apk/release/mazerunner-release.apk"
-    And I clear the "com.bugsnag.android.mazerunner" Android app data
-    And I set environment variable "BUGSNAG_API_KEY" to "a35a2a72bd230ac0aa0f52715bbdc6aa"
-    And I set environment variable "EVENT_TYPE" to "#{event_type}"
-    And I start the "com.bugsnag.android.mazerunner" Android app using the "com.bugsnag.android.mazerunner.MainActivity" activity
   }
 end
 
