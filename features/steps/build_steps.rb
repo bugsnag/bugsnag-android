fe8066cd0d8ae679500331b22ba194561c6c9575
Delisa Mason <iskanamagus@gmail.com>
2018-09-07 02:30:59 -0700

    test(maze): Update deps

--- features/steps/build_steps.rb
+++ features/steps/build_steps.rb
@@ -9,21 +9,13 @@ end
 When(/^I run "([^"]+)" against "([^"]+)"$/) do |event_type, emulator|
   wait_time = RUNNING_CI ? '10' : '5'
   steps %Q{
-    When I start emulator "#{emulator}"
+    When I start Android emulator "#{emulator}"
     And I install the "com.bugsnag.android.mazerunner" Android app from "features/fixtures/mazerunner/build/outputs/apk/release/mazerunner-release.apk"
     And I clear the "com.bugsnag.android.mazerunner" Android app data
     And I set environment variable "BUGSNAG_API_KEY" to "a35a2a72bd230ac0aa0f52715bbdc6aa"
     And I set environment variable "EVENT_TYPE" to "#{event_type}"
     And I start the "com.bugsnag.android.mazerunner" Android app using the "com.bugsnag.android.mazerunner.MainActivity" activity
     And I wait for #{wait_time} seconds
-  }
-end
-
-When("I start emulator {string}") do |emulator|
-  steps %Q{
-    When I set environment variable "ANDROID_EMULATOR" to "#{emulator}"
-    And I run the script "features/scripts/launch_emulator.sh"
-    And I run the script "await-android-emulator.sh" synchronously
   }
 end
 
