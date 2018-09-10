9fd0017d95c205663b389c52eb8239525b76ffbb
Delisa Mason <iskanamagus@gmail.com>
2018-09-10 10:23:15 -0700

    refactor: Move the JNI lookup logic to the beginning
    Also separates static field lookups into separate methods for clarity

--- ndk/src/main/jni/bugsnag.c
+++ ndk/src/main/jni/bugsnag.c
@@ -41,23 +41,43 @@ void bugsnag_leave_breadcrumb(char *name, bsg_breadcrumb_t type) {
   }
 }
 
+jfieldID bsg_parse_jseverity(JNIEnv *env, bsg_severity_t severity,
+                             jclass severity_class) {
+  const char *severity_sig = "Lcom/bugsnag/android/Severity;";
+  if (severity == BSG_SEVERITY_ERR) {
+    return (*env)->GetStaticFieldID(env, severity_class, "ERROR", severity_sig);
+  } else if (severity == BSG_SEVERITY_WARN) {
+    return (*env)->GetStaticFieldID(env, severity_class, "WARNING",
+                                    severity_sig);
+  } else {
+    return (*env)->GetStaticFieldID(env, severity_class, "INFO", severity_sig);
+  }
+}
+
 void bugsnag_notify_env(JNIEnv *env, char *name, char *message,
                         bsg_severity_t severity) {
   bsg_stackframe stacktrace[BUGSNAG_FRAMES_MAX];
   ssize_t frame_count =
       bsg_unwind_stack(bsg_configured_unwind_style(), stacktrace, NULL, NULL);
 
+  jclass interface_class =
+      (*env)->FindClass(env, "com/bugsnag/android/NativeInterface");
+  jmethodID notify_method = (*env)->GetStaticMethodID(
+      env, interface_class, "notify",
+      "(Ljava/lang/String;Ljava/lang/String;Lcom/bugsnag/android/"
+      "Severity;[Ljava/lang/StackTraceElement;)V");
   jclass trace_class = (*env)->FindClass(env, "java/lang/StackTraceElement");
+  jclass severity_class =
+      (*env)->FindClass(env, "com/bugsnag/android/Severity");
   jmethodID trace_constructor = (*env)->GetMethodID(
       env, trace_class, "<init>",
       "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;I)V");
   jobjectArray trace = (*env)->NewObjectArray(
-      env, frame_count, (*env)->FindClass(env, "java/lang/StackTraceElement"),
-      NULL);
+      env, (jsize)frame_count,
+      (*env)->FindClass(env, "java/lang/StackTraceElement"), NULL);
 
   for (int i = 0; i < frame_count; i++) {
     bsg_stackframe frame = stacktrace[i];
-
     jstring class = (*env)->NewStringUTF(env, "");
     jstring filename = (*env)->NewStringUTF(env, frame.filename);
     jstring method = (*env)->NewStringUTF(env, frame.method);
@@ -71,31 +91,12 @@ void bugsnag_notify_env(JNIEnv *env, char *name, char *message,
   }
 
   // Create a severity Error
-  jclass severity_class =
-      (*env)->FindClass(env, "com/bugsnag/android/Severity");
-  jfieldID error_field;
-  if (severity == BSG_SEVERITY_ERR) {
-    error_field = (*env)->GetStaticFieldID(env, severity_class, "ERROR",
-                                           "Lcom/bugsnag/android/Severity;");
-  } else if (severity == BSG_SEVERITY_WARN) {
-    error_field = (*env)->GetStaticFieldID(env, severity_class, "WARNING",
-                                           "Lcom/bugsnag/android/Severity;");
-  } else {
-    error_field = (*env)->GetStaticFieldID(env, severity_class, "INFO",
-                                           "Lcom/bugsnag/android/Severity;");
-  }
-  jobject jseverity =
-      (*env)->GetStaticObjectField(env, severity_class, error_field);
+  jobject jseverity = (*env)->GetStaticObjectField(
+      env, severity_class, bsg_parse_jseverity(env, severity, severity_class));
 
   jstring jname = (*env)->NewStringUTF(env, name);
   jstring jmessage = (*env)->NewStringUTF(env, message);
 
-  jclass interface_class =
-      (*env)->FindClass(env, "com/bugsnag/android/NativeInterface");
-  jmethodID notify_method = (*env)->GetStaticMethodID(
-      env, interface_class, "notify",
-      "(Ljava/lang/String;Ljava/lang/String;Lcom/bugsnag/android/"
-      "Severity;[Ljava/lang/StackTraceElement;)V");
   (*env)->CallStaticVoidMethod(env, interface_class, notify_method, jname,
                                jmessage, jseverity, trace);
 
@@ -126,6 +127,28 @@ void bugsnag_set_user_env(JNIEnv *env, char *id, char *email, char *name) {
   (*env)->DeleteLocalRef(env, interface_class);
 }
 
+jfieldID bsg_parse_jcrumb_type(JNIEnv *env, bsg_breadcrumb_t type,
+                               jclass type_class) {
+  const char *type_sig = "Lcom/bugsnag/android/BreadcrumbType;";
+  if (type == BSG_CRUMB_USER) {
+    return (*env)->GetStaticFieldID(env, type_class, "USER", type_sig);
+  } else if (type == BSG_CRUMB_ERROR) {
+    return (*env)->GetStaticFieldID(env, type_class, "ERROR", type_sig);
+  } else if (type == BSG_CRUMB_LOG) {
+    return (*env)->GetStaticFieldID(env, type_class, "LOG", type_sig);
+  } else if (type == BSG_CRUMB_NAVIGATION) {
+    return (*env)->GetStaticFieldID(env, type_class, "NAVIGATION", type_sig);
+  } else if (type == BSG_CRUMB_PROCESS) {
+    return (*env)->GetStaticFieldID(env, type_class, "PROCESS", type_sig);
+  } else if (type == BSG_CRUMB_REQUEST) {
+    return (*env)->GetStaticFieldID(env, type_class, "REQUEST", type_sig);
+  } else if (type == BSG_CRUMB_STATE) {
+    return (*env)->GetStaticFieldID(env, type_class, "STATE", type_sig);
+  } else { // MANUAL is the default type
+    return (*env)->GetStaticFieldID(env, type_class, "MANUAL", type_sig);
+  }
+}
+
 void bugsnag_leave_breadcrumb_env(JNIEnv *env, char *name,
                                   bsg_breadcrumb_t type) {
   jclass interface_class =
@@ -133,37 +156,11 @@ void bugsnag_leave_breadcrumb_env(JNIEnv *env, char *name,
   jmethodID leave_breadcrumb_method = (*env)->GetStaticMethodID(
       env, interface_class, "leaveBreadcrumb",
       "(Ljava/lang/String;Lcom/bugsnag/android/BreadcrumbType;)V");
-
   jclass type_class =
       (*env)->FindClass(env, "com/bugsnag/android/BreadcrumbType");
-  jfieldID type_field;
-  if (type == BSG_CRUMB_USER) {
-    type_field = (*env)->GetStaticFieldID(
-        env, type_class, "USER", "Lcom/bugsnag/android/BreadcrumbType;");
-  } else if (type == BSG_CRUMB_ERROR) {
-    type_field = (*env)->GetStaticFieldID(
-        env, type_class, "ERROR", "Lcom/bugsnag/android/BreadcrumbType;");
-  } else if (type == BSG_CRUMB_LOG) {
-    type_field = (*env)->GetStaticFieldID(
-        env, type_class, "LOG", "Lcom/bugsnag/android/BreadcrumbType;");
-  } else if (type == BSG_CRUMB_NAVIGATION) {
-    type_field = (*env)->GetStaticFieldID(
-        env, type_class, "NAVIGATION", "Lcom/bugsnag/android/BreadcrumbType;");
-  } else if (type == BSG_CRUMB_PROCESS) {
-    type_field = (*env)->GetStaticFieldID(
-        env, type_class, "PROCESS", "Lcom/bugsnag/android/BreadcrumbType;");
-  } else if (type == BSG_CRUMB_REQUEST) {
-    type_field = (*env)->GetStaticFieldID(
-        env, type_class, "REQUEST", "Lcom/bugsnag/android/BreadcrumbType;");
-  } else if (type == BSG_CRUMB_STATE) {
-    type_field = (*env)->GetStaticFieldID(
-        env, type_class, "STATE", "Lcom/bugsnag/android/BreadcrumbType;");
-  } else { // MANUAL is the default type
-    type_field = (*env)->GetStaticFieldID(
-        env, type_class, "MANUAL", "Lcom/bugsnag/android/BreadcrumbType;");
-  }
 
-  jobject jtype = (*env)->GetStaticObjectField(env, type_class, type_field);
+  jobject jtype = (*env)->GetStaticObjectField(
+      env, type_class, bsg_parse_jcrumb_type(env, type, type_class));
   jstring jname = (*env)->NewStringUTF(env, name);
   (*env)->CallStaticVoidMethod(env, interface_class, leave_breadcrumb_method,
                                jname, jtype);
