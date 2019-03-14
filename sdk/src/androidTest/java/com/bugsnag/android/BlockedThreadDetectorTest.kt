07369917b5c15180e74cc05ce05dd4c72723d38c
fractalwrench <fractalwrench@gmail.com>
2019-03-14 09:48:52 +0000

    test: add tests to verify the default behaviour of the ANR config and BlockedThreadDetector

---
+++ sdk/src/androidTest/java/com/bugsnag/android/BlockedThreadDetectorTest.kt
@@ -0,0 +1,34 @@
+package com.bugsnag.android
+
+import android.os.Looper
+import org.junit.Test
+
+class BlockedThreadDetectorTest {
+
+    private val looper = Looper.getMainLooper()
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testInvalidBlockedThresholdMs() {
+        BlockedThreadDetector(-1, 1, looper) {}
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testInvalidCheckIntervalMs() {
+        BlockedThreadDetector(1, -1, looper) {}
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testInvalidThread() {
+        BlockedThreadDetector(1, 1, null) {}
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testInvalidDelegate() {
+        BlockedThreadDetector(1, 1, looper, null)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testExcessiveCheckInterval() {
+        BlockedThreadDetector(100, 1000, looper) {}
+    }
+}
