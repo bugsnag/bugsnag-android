d1a7beae09f4da792e20684f901e2e20e54fbfba
fractalwrench <fractalwrench@gmail.com>
2019-05-15 15:31:06 +0100

    test: address flaky unit tests found on browserstack

--- sdk/src/androidTest/java/com/bugsnag/android/BeforeSendSessionTest.kt
+++ sdk/src/androidTest/java/com/bugsnag/android/BeforeSendSessionTest.kt
@@ -1,28 +1,24 @@
 package com.bugsnag.android
 
+import android.support.test.InstrumentationRegistry
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 class BeforeSendSessionTest {
-
-    private val client = BugsnagTestUtils.generateClient()
-    private val sessionTracker = client.sessionTracker
-
-    @Before
-    fun setUp() {
-    }
 
     @Test
     fun testBeforeSendCallbackInvoked() {
         val latch = CountDownLatch(1)
         var data: SessionTrackingPayload? = null
 
-        client.config.addBeforeSendSession { it.device["foo"] = "bar" }
-        client.config.addBeforeSendSession { it.device["foo2"] = 5 }
+        val config = Configuration("api-key")
+        config.addBeforeSendSession { it.device["foo"] = "bar" }
+        config.addBeforeSendSession { it.device["foo2"] = 5 }
 
-        client.config.delivery = object : Delivery {
+        config.delivery = object : Delivery {
             override fun deliver(payload: SessionTrackingPayload, config: Configuration) {
                 data = payload
                 latch.countDown()
@@ -31,8 +27,9 @@ class BeforeSendSessionTest {
             override fun deliver(report: Report, config: Configuration) {}
         }
 
-        sessionTracker.startSession(false)
-        latch.await()
+        val client = Client(InstrumentationRegistry.getContext(), config)
+        client.sessionTracker.startSession(false)
+        latch.await(100, TimeUnit.MILLISECONDS)
 
         assertEquals("bar", data!!.device["foo"])
         assertEquals(5, data!!.device["foo2"])
