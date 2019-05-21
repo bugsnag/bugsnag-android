41f1f6670d7b99619983b88aae31a8198808f6b4
Alex Moinet <alex@bugsnag.com>
2019-05-21 12:49:43 +0100

    Buildkite-move: Make scenarios runnable through appium

--- features/fixtures/mazerunner/src/main/java/com/bugsnag/android/mazerunner/MainActivity.kt
+++ features/fixtures/mazerunner/src/main/java/com/bugsnag/android/mazerunner/MainActivity.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import com.bugsnag.android.Bugsnag
 import com.bugsnag.android.Configuration
 import com.bugsnag.android.mazerunner.scenarios.Scenario
@@ -15,8 +17,21 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val scenarioStarter = findViewById<Button>(R.id.startScenarioButton)
+
+        scenarioStarter.setOnClickListener {
+            val scenarioPicker = findViewById<EditText>(R.id.scenarioText)
+            val scenario = scenarioPicker.text.toString()
+            val scenarioMetaData = findViewById<EditText>(R.id.scenarioMetaData)
+            val metaData = scenarioMetaData.text.toString()
+            executeScenario(scenario, metaData)
+        }
+    }
+
+    private fun executeScenario(eventType: String, metaData: String) {
         val config = prepareConfig()
-        val testCase = loadScenario(config)
+        val testCase = loadScenario(config, eventType, metaData)
 
         Bugsnag.init(this, config)
         Bugsnag.setLoggingEnabled(true)
@@ -30,9 +45,7 @@ class MainActivity : Activity() {
         }, 1)
     }
 
-    private fun loadScenario(configuration: Configuration): Scenario {
-        val eventType = intent.getStringExtra("EVENT_TYPE")
-        val eventMetaData = intent.getStringExtra("EVENT_METADATA")
+    private fun loadScenario(configuration: Configuration, eventType: String, eventMetaData: String): Scenario {
         Log.d("Bugsnag", "Received test case, executing " + eventType)
 
         val testCase = factory.testCaseForName(eventType, configuration, this)
@@ -43,19 +56,9 @@ class MainActivity : Activity() {
 
     private fun prepareConfig(): Configuration {
         val config = Configuration(intent.getStringExtra("BUGSNAG_API_KEY"))
-        val port = intent.getStringExtra("BUGSNAG_PORT")
-        config.setEndpoints("${findHostname()}:$port", "${findHostname()}:$port")
+        config.setEndpoints("http://10.0.2.2:9339", "http://10.0.2.2:9339")
         config.detectAnrs = false
         return config
-    }
-
-    private fun findHostname(): String {
-        val isEmulator = Build.FINGERPRINT.startsWith("unknown")
-            || Build.FINGERPRINT.contains("generic")
-        return when {
-            isEmulator -> "http://10.0.2.2"
-            else -> "http://localhost"
-        }
     }
 
 }
