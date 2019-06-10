c09d0e6bdda2a68d0cd7a2030ed85f7431032adf
Alex Moinet <alex@bugsnag.com>
2019-06-10 08:09:07 +0100

    Buildkite-move: Adjust timings to help with error dialogue

--- features/steps/android_steps.rb
+++ features/steps/android_steps.rb
@@ -15,20 +15,22 @@ When("I run {string} and relaunch the app") do |event_type|
 end
 
 When("I clear any error dialogue") do
+  # Unfortunately necessary as the error dialogue doesn't show straight away
+  sleep(3000)
   begin
-    $driver.wait_for_element("android:id/button1", 3)
+    $driver.wait_for_element("android:id/button1", 1)
   rescue Selenium::WebDriver::Error::TimeoutError
   else
     $driver.click_element("android:id/button1")
   end
   begin
-    $driver.wait_for_element("android:id/aerr_close", 3)
+    $driver.wait_for_element("android:id/aerr_close", 1)
   rescue Selenium::WebDriver::Error::TimeoutError
   else
     $driver.click_element("android:id/aerr_close")
   end
   begin
-    $driver.wait_for_element("android:id/aerr_restart", 3)
+    $driver.wait_for_element("android:id/aerr_restart", 1)
   rescue Selenium::WebDriver::Error::TimeoutError
   else
     $driver.click_element("android:id/aerr_restart")
