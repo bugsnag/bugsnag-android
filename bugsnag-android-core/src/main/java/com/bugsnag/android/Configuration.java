a247e2aae0b6bf7ded57b02a4f39f59e0d479bbf
fractalwrench <fractalwrench@gmail.com>
2019-07-15 15:24:30 +0100

    refactor: set DETECT_NDK_CRASHES in bugsnag-android-ndk artefact

--- bugsnag-android-core/src/main/java/com/bugsnag/android/Configuration.java
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/Configuration.java
@@ -1,11 +1,11 @@
 package com.bugsnag.android;
 
 import android.content.Context;
-import android.os.Debug;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
@@ -49,7 +49,7 @@ public class Configuration extends Observable implements Observer {
     private boolean automaticallyCollectBreadcrumbs = true;
 
     private boolean detectAnrs = false;
-    private boolean detectNdkCrashes = false;
+    private boolean detectNdkCrashes;
     private long anrThresholdMs = 5000;
 
     @NonNull
@@ -75,6 +75,15 @@ public class Configuration extends Observable implements Observer {
         this.apiKey = apiKey;
         this.metaData = new MetaData();
         this.metaData.addObserver(this);
+
+        try {
+            // check if DETECT_NDK_CRASHES has been set in bugsnag-android or bugsnag-android-ndk
+            Class<?> clz = Class.forName("com.bugsnag.android.BuildConfig");
+            Field field = clz.getDeclaredField("DETECT_NDK_CRASHES");
+            detectNdkCrashes = field.getBoolean(null);
+        } catch (Throwable exc) {
+            detectNdkCrashes = false;
+        }
     }
 
     /**
