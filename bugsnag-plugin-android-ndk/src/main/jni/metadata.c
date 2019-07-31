d2211322c3a6c5bc0a028b91213fb9b58e868d1a
Jamie Lynch <fractalwrench@gmail.com>
2019-07-31 15:16:03 +0100

    Apply suggestions from code review
    Co-Authored-By: Delisa <kattrali@users.noreply.github.com>

--- bugsnag-plugin-android-ndk/src/main/jni/metadata.c
+++ bugsnag-plugin-android-ndk/src/main/jni/metadata.c
@@ -188,9 +188,9 @@ void bsg_populate_crumb_metadata(JNIEnv *env, bugsnag_breadcrumb *crumb,
       (*env)->CallObjectMethod(env, metadata, jni_cache->map_key_set);
   jobject keylist = (*env)->NewObject(
       env, jni_cache->arraylist, jni_cache->arraylist_init_with_obj, keyset);
-  size_t metadataSize = sizeof(crumb->metadata) / sizeof(bsg_char_metadata_pair);
+  size_t metadata_size = sizeof(crumb->metadata) / sizeof(bsg_char_metadata_pair);
 
-  for (int i = 0; i < map_size && i < metadataSize; i++) {
+  for (int i = 0; i < map_size && i < metadata_size; i++) {
     jstring _key = (*env)->CallObjectMethod(env, keylist,
                                             jni_cache->arraylist_get, (jint)i);
     jstring _value =
