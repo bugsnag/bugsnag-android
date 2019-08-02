d975c066168f9f75f31bbf379c7f1dcdf427cd15
fractalwrench <fractalwrench@gmail.com>
2019-08-02 11:31:40 +0100

    docs: update delivery javadoc

--- bugsnag-android-core/src/main/java/com/bugsnag/android/Delivery.kt
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/Delivery.kt
@@ -19,37 +19,47 @@ interface Delivery {
     /**
      * Posts an array of sessions to the Bugsnag Session Tracking API.
      *
-     *
-     * This request must be delivered to the endpoint specified in deliveryParams with the given
+     * This request must be delivered to the endpoint specified in [deliveryParams] with the given
      * HTTP headers.
      *
+     * You should return the [DeliveryStatus] which best matches the end-result of your delivery
+     * attempt. Bugsnag will use the return value to decide whether to delete the payload if it was
+     * cached on disk, or whether to reattempt delivery later on.
      *
-     * You should return the status code of the network request made within your Delivery
-     * implementation. A 2xx status will indicate success, a 4xx status will remove the cached
-     * payload from disk, and a 5xx status will save the payload and attempt delivery again later.
+     * For example, a 2xx status code will indicate success so you should return
+     * [DeliveryStatus.DELIVERED]. Most 4xx status codes would indicate an unrecoverable error, so
+     * the report should be dropped using [DeliveryStatus.FAILURE]. For all other scenarios,
+     * delivery should be attempted again later by using [DeliveryStatus.UNDELIVERED].
      *
      * See [https://docs.bugsnag.com/api/sessions/](https://docs.bugsnag.com/api/sessions/)
      *
      * @param payload        The session tracking payload
      * @param deliveryParams The delivery parameters to be used for this request
+     * @return the end-result of your delivery attempt
      */
     fun deliver(payload: SessionTrackingPayload, deliveryParams: DeliveryParams): DeliveryStatus
 
     /**
      * Posts an Error Report to the Bugsnag Error Reporting API.
      *
-     * This request must be delivered to the endpoint specified in deliveryParams with the given
+     * This request must be delivered to the endpoint specified in [deliveryParams] with the given
      * HTTP headers.
      *
-     * You should return the status code of the network request made within your Delivery
-     * implementation. A 2xx status will indicate success, a 4xx status will remove the cached
-     * payload from disk, and a 5xx status will save the payload and attempt delivery again later.
+     * You should return the [DeliveryStatus] which best matches the end-result of your delivery
+     * attempt. Bugsnag will use the return value to decide whether to delete the payload if it was
+     * cached on disk, or whether to reattempt delivery later on.
+     *
+     * For example, a 2xx status code will indicate success so you should return
+     * [DeliveryStatus.DELIVERED]. Most 4xx status codes would indicate an unrecoverable error, so
+     * the report should be dropped using [DeliveryStatus.FAILURE]. For all other scenarios,
+     * delivery should be attempted again later by using [DeliveryStatus.UNDELIVERED].
      *
      * See [https://docs.bugsnag.com/api/error-reporting/]
      * (https://docs.bugsnag.com/api/error-reporting/)
      *
      * @param report         The error report
      * @param deliveryParams The delivery parameters to be used for this request
+     * @return the end-result of your delivery attempt
      */
     fun deliver(report: Report, deliveryParams: DeliveryParams): DeliveryStatus
 }
