e22b2715224216ae65eb16411270f5628b306a4b
fractalwrench <fractalwrench@gmail.com>
2019-08-02 14:05:28 +0100

    refactor: update default delivery to increase logging

--- bugsnag-android-core/src/main/java/com/bugsnag/android/DefaultDelivery.kt
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/DefaultDelivery.kt
@@ -4,14 +4,17 @@ import java.io.BufferedWriter
 import java.io.IOException
 import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
-import java.net.HttpURLConnection.*
+import java.net.HttpURLConnection.HTTP_BAD_REQUEST
+import java.net.HttpURLConnection.HTTP_CLIENT_TIMEOUT
+import java.net.HttpURLConnection.HTTP_OK
 import java.net.URL
 import java.nio.charset.Charset
 
 internal class DefaultDelivery(private val connectivity: Connectivity?) : Delivery {
 
     override fun deliver(payload: SessionTrackingPayload,
-                         deliveryParams: DeliveryParams): DeliveryStatus {
+        deliveryParams: DeliveryParams
+    ): DeliveryStatus {
         val status = deliver(deliveryParams.endpoint, payload, deliveryParams.headers)
         Logger.info("Session API request finished with status $status")
         return status
@@ -58,7 +61,10 @@ internal class DefaultDelivery(private val connectivity: Connectivity?) : Delive
             }
 
             // End the request, get the response code
-            return getDeliveryStatus(conn.responseCode)
+            val responseCode = conn.responseCode
+            val status = getDeliveryStatus(responseCode)
+            logRequestInfo(responseCode, conn, status)
+            return status
         } catch (exception: IOException) {
             Logger.warn("IOException encountered in request", exception)
             return DeliveryStatus.UNDELIVERED
@@ -70,9 +76,22 @@ internal class DefaultDelivery(private val connectivity: Connectivity?) : Delive
         }
     }
 
+    private fun logRequestInfo(code: Int, conn: HttpURLConnection, status: DeliveryStatus) {
+        Logger.info(
+            "Request completed with code $code, " +
+                    "message: ${conn.responseMessage}, " +
+                    "headers: ${conn.headerFields}"
+        )
+
+        if (status != DeliveryStatus.DELIVERED) {
+            val errBody = conn.errorStream.bufferedReader().readText()
+            Logger.warn("Request error details: $errBody")
+        }
+    }
+
     internal fun getDeliveryStatus(responseCode: Int): DeliveryStatus {
         val unrecoverableCodes = IntRange(HTTP_BAD_REQUEST, 499).filter {
-            it !=  HTTP_CLIENT_TIMEOUT && it != 429
+            it != HTTP_CLIENT_TIMEOUT && it != 429
         }
 
         return when (responseCode) {
