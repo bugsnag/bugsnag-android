3a86807d55d3d172ec24d5021195ca9e9ad28fa0
fractalwrench <fractalwrench@gmail.com>
2019-08-23 13:03:58 +0100

    test: update internal error report test to trigger error from corrupted json file

--- tests/features/fixtures/mazerunner/src/main/java/com/bugsnag/android/mazerunner/scenarios/InternalReportScenario.kt
+++ tests/features/fixtures/mazerunner/src/main/java/com/bugsnag/android/mazerunner/scenarios/InternalReportScenario.kt
@@ -1,17 +1,24 @@
 package com.bugsnag.android.mazerunner.scenarios
 
 import android.content.Context
-import com.bugsnag.android.*
+import com.bugsnag.android.Bugsnag
+import com.bugsnag.android.Configuration
+import java.io.File
 
+/**
+ * Sends a handled exception to Bugsnag, which does not include session data.
+ */
 internal class InternalReportScenario(config: Configuration,
                                       context: Context) : Scenario(config, context) {
+
     init {
         config.setAutoCaptureSessions(false)
+        disableAllDelivery(config)
     }
 
     override fun run() {
         super.run()
-        sendInternalReport(RuntimeException("Whoops"), config, Bugsnag.getClient())
+        Bugsnag.notify(java.lang.RuntimeException("Whoops"))
     }
 
 }
--- tests/features/internal_report.feature
+++ tests/features/internal_report.feature
@@ -1,11 +1,12 @@
 Feature: Sending internal error reports
 
-Scenario: Send a report about an error triggered within the notifier
-    When I run "InternalReportScenario"
-    Then I should receive 1 request
+Scenario: Sending internal error reports
+    When I run "InternalReportScenario" and relaunch the app
+    And I configure Bugsnag for "EmptyReportScenario"
+    And I wait to receive a request
     And the "Bugsnag-Internal-Error" header equals "true"
     And the payload field "apiKey" is null
-    And the event "context" is null
+    And the event "context" equals "Crash Report Deserialization"
     And the event "session" is null
     And the event "breadcrumbs" is null
     And the event "app.type" equals "android"
