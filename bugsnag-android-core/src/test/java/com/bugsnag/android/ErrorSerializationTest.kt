305a9e9100048564a177bab8815ba4e75a6bacfa
fractalwrench <fractalwrench@gmail.com>
2019-10-01 09:55:13 +0100

    test: add Error JVM serialization test

---
+++ bugsnag-android-core/src/test/java/com/bugsnag/android/ErrorSerializationTest.kt
@@ -0,0 +1,42 @@
+package com.bugsnag.android
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameter
+import org.junit.runners.Parameterized.Parameters
+
+@RunWith(Parameterized::class)
+internal class ErrorSerializationTest {
+
+    companion object {
+
+        @JvmStatic
+        @Parameters
+        fun testCases(): Collection<Pair<Error, String>> {
+            val handledState = HandledState.newInstance(HandledState.REASON_UNHANDLED_EXCEPTION)
+            val config = Configuration("api-key")
+            val frame = StackTraceElement("Foo.kt", "bar", "Foo.kt", 55)
+            val cachedThread = CachedThread(config, 1, "my-thread", "android", true, arrayOf(frame))
+            val threadState = ThreadState(arrayOf(cachedThread))
+
+            return generateTestCases(
+                "error",
+                Error(
+                    config,
+                    RuntimeException("whoops"),
+                    handledState,
+                    Severity.ERROR,
+                    null,
+                    threadState
+                )
+            )
+        }
+    }
+
+    @Parameter
+    lateinit var testCase: Pair<Error, String>
+
+    @Test
+    fun testJsonSerialisation() = verifyJsonMatches(testCase.first, testCase.second)
+}
