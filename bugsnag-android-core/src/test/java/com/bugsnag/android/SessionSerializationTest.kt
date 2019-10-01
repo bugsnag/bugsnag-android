ec584c214e8609719df766746fb27fb6f29c7861
fractalwrench <fractalwrench@gmail.com>
2019-10-01 08:42:34 +0100

    test: add Session JVM serialization test

---
+++ bugsnag-android-core/src/test/java/com/bugsnag/android/SessionSerializationTest.kt
@@ -0,0 +1,27 @@
+package com.bugsnag.android
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameter
+import org.junit.runners.Parameterized.Parameters
+import java.util.Date
+
+@RunWith(Parameterized::class)
+internal class SessionSerializationTest {
+
+    companion object {
+        @JvmStatic
+        @Parameters
+        fun testCases() = generateTestCases(
+            "session",
+            Session("123", Date(0), User(), 1, 0)
+        )
+    }
+
+    @Parameter
+    lateinit var testCase: Pair<Session, String>
+
+    @Test
+    fun testJsonSerialisation() = verifyJsonMatches(testCase.first, testCase.second)
+}
