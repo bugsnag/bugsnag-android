e0946fb2a2604e8b68eb4614bca930b64157589b
fractalwrench <fractalwrench@gmail.com>
2019-10-01 09:07:46 +0100

    test: add Stacktrace JVM serialization test

---
+++ bugsnag-android-core/src/test/java/com/bugsnag/android/StacktraceSerializationTest.kt
@@ -0,0 +1,23 @@
+package com.bugsnag.android
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameter
+import org.junit.runners.Parameterized.Parameters
+
+@RunWith(Parameterized::class)
+internal class StacktraceSerializationTest {
+
+    companion object {
+        @JvmStatic
+        @Parameters
+        fun testCases() = generateTestCases("stacktrace", Stacktrace(arrayOf(), arrayOf()))
+    }
+
+    @Parameter
+    lateinit var testCase: Pair<Stacktrace, String>
+
+    @Test
+    fun testJsonSerialisation() = verifyJsonMatches(testCase.first, testCase.second)
+}
