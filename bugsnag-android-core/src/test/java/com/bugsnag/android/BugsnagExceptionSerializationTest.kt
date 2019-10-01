a0a8c664423c854fbd5b7cfb13e0db50c6aa0676
fractalwrench <fractalwrench@gmail.com>
2019-10-01 09:16:54 +0100

    test: add BugsnagException JVM serialization test

---
+++ bugsnag-android-core/src/test/java/com/bugsnag/android/BugsnagExceptionSerializationTest.kt
@@ -0,0 +1,27 @@
+package com.bugsnag.android
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameter
+import org.junit.runners.Parameterized.Parameters
+
+@RunWith(Parameterized::class)
+internal class BugsnagExceptionSerializationTest {
+
+    companion object {
+        @JvmStatic
+        @Parameters
+        fun testCases(): Collection<Pair<JsonStream.Streamable, String>> {
+            val trace = arrayOf(StackTraceElement("Foo", "bar", "Foo.kt", 1))
+            val exc = BugsnagException("MyClass", "Custom message", trace)
+            return generateTestCases("bugsnag_exception", exc)
+        }
+    }
+
+    @Parameter
+    lateinit var testCase: Pair<BugsnagException, String>
+
+    @Test
+    fun testJsonSerialisation() = verifyJsonMatches(testCase.first, testCase.second)
+}
