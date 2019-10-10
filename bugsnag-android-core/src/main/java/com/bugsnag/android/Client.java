cb68a5d22d5e6224a187f99361c60cc1220657af
fractalwrench <fractalwrench@gmail.com>
2019-10-10 15:08:16 +0100

    add usable space back into bugsnagdiagnostics tab

--- bugsnag-android-core/src/main/java/com/bugsnag/android/Client.java
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/Client.java
@@ -215,6 +215,9 @@ public class Client extends Observable implements Observer {
                 metaData.addToTab(INTERNAL_DIAGNOSTICS_TAB, "canWrite", errorFile.canWrite());
                 metaData.addToTab(INTERNAL_DIAGNOSTICS_TAB, "exists", errorFile.exists());
 
+                @SuppressLint("UsableSpace") // storagemanager alternative API requires API 26
+                long usableSpace = appContext.getCacheDir().getUsableSpace();
+                metaData.addToTab(INTERNAL_DIAGNOSTICS_TAB, "usableSpace", usableSpace);
                 metaData.addToTab(INTERNAL_DIAGNOSTICS_TAB, "filename", errorFile.getName());
                 metaData.addToTab(INTERNAL_DIAGNOSTICS_TAB, "fileLength", errorFile.length());
                 recordStorageCacheBehavior(metaData);
