285c448be7245417cf54696bfe1e263d541ee382
fractalwrench <fractalwrench@gmail.com>
2019-10-17 16:39:34 +0100

    fix: add missing import

--- bugsnag-android-core/src/main/java/com/bugsnag/android/Configuration.java
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/Configuration.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
