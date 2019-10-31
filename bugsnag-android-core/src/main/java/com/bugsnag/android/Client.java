e02924c7639f892727ad7b2360e04877367581ae
fractalwrench <fractalwrench@gmail.com>
2019-10-31 16:58:38 +0000

    refactor: extract report delivery into client delegate

--- bugsnag-android-core/src/main/java/com/bugsnag/android/Client.java
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/Client.java
@@ -53,7 +53,6 @@ public class Client extends Observable implements Observer, MetadataAware, Callb
 
     private static final String SHARED_PREF_KEY = "com.bugsnag.android";
 
-
     final ImmutableConfig immutableConfig;
 
     final CallbackState callbackState;
@@ -85,6 +84,7 @@ public class Client extends Observable implements Observer, MetadataAware, Callb
     private final Connectivity connectivity;
     private final UserRepository userRepository;
     final Logger logger;
+    final ReportDeliveryDelegate reportDeliveryDelegate;
 
     /**
      * Initialize a Bugsnag client
@@ -173,6 +173,10 @@ public class Client extends Observable implements Observer, MetadataAware, Callb
         InternalReportDelegate delegate = new InternalReportDelegate(appContext,
                 logger, immutableConfig, storageManager, appData, deviceData, sessionTracker);
         eventStore = new EventStore(immutableConfig, appContext, logger, delegate);
+
+        reportDeliveryDelegate = new ReportDeliveryDelegate(logger, eventStore,
+                immutableConfig, breadcrumbState);
+        reportDeliveryDelegate.addObserver(this);
 
         // Install a default exception handler with this client
         if (immutableConfig.getAutoDetectErrors()) {
@@ -383,9 +387,7 @@ public class Client extends Observable implements Observer, MetadataAware, Callb
      */
     @Override
     public void setUser(@Nullable String id, @Nullable String email, @Nullable String name) {
-        setUserId(id);
-        setUserEmail(email);
-        setUserName(name);
+        userState.setUser(id, email, name);
     }
 
     /**
@@ -627,79 +629,7 @@ public class Client extends Observable implements Observer, MetadataAware, Callb
             logger.i("Skipping notification - onError task returned false");
             return;
         }
-
-        // increment handled counts after error not rejected by callbacks
-        Session session = event.getSession();
-
-        if (session != null) {
-            if (event.getUnhandled()) {
-                event.setSession(session.incrementUnhandledAndCopy());
-            } else {
-                event.setSession(session.incrementHandledAndCopy());
-            }
-        }
-
-        // Build the report
-        Report report = new Report(event.getApiKey(), null, event);
-
-        if (event.getSession() != null) {
-            setChanged();
-
-            if (event.getUnhandled()) {
-                notifyObservers(new Message(
-                    NativeInterface.MessageType.NOTIFY_UNHANDLED, null));
-            } else {
-                List<Error> errors = event.getErrors();
-
-                String name = "";
-
-                if (errors.size() > 0) {
-                    name = errors.get(0).getErrorClass();
-                }
-                notifyObservers(new Message(NativeInterface.MessageType.NOTIFY_HANDLED, name));
-            }
-        }
-
-        if (event.getUnhandled()) {
-            eventStore.write(event);
-            eventStore.flushAsync();
-        } else {
-            deliverReportAsync(event, report);
-        }
-    }
-
-    private void deliverReportAsync(@NonNull Event event, Report report) {
-        final Report finalReport = report;
-        final Event finalEvent = event;
-
-        // Attempt to send the report in the background
-        try {
-            Async.run(new Runnable() {
-                @Override
-                public void run() {
-                    deliver(finalReport, finalEvent);
-                }
-            });
-        } catch (RejectedExecutionException exception) {
-            eventStore.write(event);
-            logger.w("Exceeded max queue count, saving to disk to send later");
-        }
-    }
-
-    private void leaveErrorBreadcrumb(@NonNull Event event) {
-        // Add a breadcrumb for this event occurring
-        List<Error> errors = event.getErrors();
-
-        String name = "";
-        String msg = "";
-
-        if (errors.size() > 0) {
-            name = errors.get(0).getErrorClass();
-            msg = errors.get(0).getErrorMessage();
-        }
-
-        Map<String, Object> message = Collections.<String, Object>singletonMap("message", msg);
-        breadcrumbState.add(new Breadcrumb(name, BreadcrumbType.ERROR, message, new Date()));
+        reportDeliveryDelegate.deliverReport(event);
     }
 
     @Override
@@ -757,30 +687,6 @@ public class Client extends Observable implements Observer, MetadataAware, Callb
     private void leaveBreadcrumbInternal(Breadcrumb crumb) {
         if (callbackState.runOnBreadcrumbTasks(crumb, logger)) {
             breadcrumbState.add(crumb);
-        }
-    }
-
-    void deliver(@NonNull Report report, @NonNull Event event) {
-        DeliveryParams deliveryParams = immutableConfig.errorApiDeliveryParams();
-        Delivery delivery = immutableConfig.getDelivery();
-        DeliveryStatus deliveryStatus = delivery.deliver(report, deliveryParams);
-
-        switch (deliveryStatus) {
-            case DELIVERED:
-                logger.i("Sent 1 new event to Bugsnag");
-                leaveErrorBreadcrumb(event);
-                break;
-            case UNDELIVERED:
-                logger.w("Could not send event(s) to Bugsnag,"
-                        + " saving to disk to send later");
-                eventStore.write(event);
-                leaveErrorBreadcrumb(event);
-                break;
-            case FAILURE:
-                logger.w("Problem sending event to Bugsnag");
-                break;
-            default:
-                break;
         }
     }
 
---
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/ReportDeliveryDelegate.java
@@ -0,0 +1,126 @@
+package com.bugsnag.android;
+
+import androidx.annotation.NonNull;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Observable;
+import java.util.concurrent.RejectedExecutionException;
+
+class ReportDeliveryDelegate extends Observable {
+
+    final Logger logger;
+    final EventStore eventStore;
+    final ImmutableConfig immutableConfig;
+    final BreadcrumbState breadcrumbState;
+
+    ReportDeliveryDelegate(Logger logger, EventStore eventStore,
+                           ImmutableConfig immutableConfig, BreadcrumbState breadcrumbState) {
+        this.logger = logger;
+        this.eventStore = eventStore;
+        this.immutableConfig = immutableConfig;
+        this.breadcrumbState = breadcrumbState;
+    }
+
+    void deliverReport(@NonNull Event event) {
+        // increment handled counts after error not rejected by callbacks
+        Session session = event.getSession();
+
+        if (session != null) {
+            if (event.getUnhandled()) {
+                event.setSession(session.incrementUnhandledAndCopy());
+            } else {
+                event.setSession(session.incrementHandledAndCopy());
+            }
+        }
+
+        // Build the report
+        Report report = new Report(event.getApiKey(), null, event);
+
+        if (event.getSession() != null) {
+            setChanged();
+
+            if (event.getUnhandled()) {
+                notifyObservers(new NativeInterface.Message(
+                        NativeInterface.MessageType.NOTIFY_UNHANDLED, null));
+            } else {
+                List<Error> errors = event.getErrors();
+
+                String name = "";
+
+                if (errors.size() > 0) {
+                    name = errors.get(0).getErrorClass();
+                }
+                notifyObservers(new NativeInterface.Message(
+                        NativeInterface.MessageType.NOTIFY_HANDLED, name));
+            }
+        }
+
+        if (event.getUnhandled()) {
+            eventStore.write(event);
+            eventStore.flushAsync();
+        } else {
+            deliverReportAsync(event, report);
+        }
+    }
+
+    private void deliverReportAsync(@NonNull Event event, Report report) {
+        final Report finalReport = report;
+        final Event finalEvent = event;
+
+        // Attempt to send the report in the background
+        try {
+            Async.run(new Runnable() {
+                @Override
+                public void run() {
+                    deliver(finalReport, finalEvent);
+                }
+            });
+        } catch (RejectedExecutionException exception) {
+            eventStore.write(event);
+            logger.w("Exceeded max queue count, saving to disk to send later");
+        }
+    }
+
+    void deliver(@NonNull Report report, @NonNull Event event) {
+        DeliveryParams deliveryParams = immutableConfig.errorApiDeliveryParams();
+        Delivery delivery = immutableConfig.getDelivery();
+        DeliveryStatus deliveryStatus = delivery.deliver(report, deliveryParams);
+
+        switch (deliveryStatus) {
+            case DELIVERED:
+                logger.i("Sent 1 new event to Bugsnag");
+                leaveErrorBreadcrumb(event);
+                break;
+            case UNDELIVERED:
+                logger.w("Could not send event(s) to Bugsnag,"
+                        + " saving to disk to send later");
+                eventStore.write(event);
+                leaveErrorBreadcrumb(event);
+                break;
+            case FAILURE:
+                logger.w("Problem sending event to Bugsnag");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void leaveErrorBreadcrumb(@NonNull Event event) {
+        // Add a breadcrumb for this event occurring
+        List<Error> errors = event.getErrors();
+
+        String name = "";
+        String msg = "";
+
+        if (errors.size() > 0) {
+            name = errors.get(0).getErrorClass();
+            msg = errors.get(0).getErrorMessage();
+        }
+
+        Map<String, Object> message = Collections.<String, Object>singletonMap("message", msg);
+        breadcrumbState.add(new Breadcrumb(name, BreadcrumbType.ERROR, message, new Date()));
+    }
+}
