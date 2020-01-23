af1afd2fc14d12940c5d0f626ec6ea3a0ecbbd29
fractalwrench <fractalwrench@gmail.com>
2020-01-23 17:04:59 +0000

    style: fix violations

--- mazerunner-scenarios/src/main/java/com/bugsnag/android/JavaHooks.java
+++ mazerunner-scenarios/src/main/java/com/bugsnag/android/JavaHooks.java
@@ -1,7 +1,5 @@
 package com.bugsnag.android;
 
-import androidx.annotation.NonNull;
-
 import com.bugsnag.android.AppWithState;
 import com.bugsnag.android.Configuration;
 import com.bugsnag.android.Delivery;
@@ -15,23 +13,35 @@ import com.bugsnag.android.NoopLogger;
 import com.bugsnag.android.Report;
 import com.bugsnag.android.SessionPayload;
 
-import org.jetbrains.annotations.NotNull;
+import androidx.annotation.NonNull;
 
 import java.util.Date;
 
 public class JavaHooks {
 
+    /**
+     * Generates fake DeviceWithState
+     */
+    @NonNull
     public static DeviceWithState generateDeviceWithState() {
         DeviceBuildInfo buildInfo = DeviceBuildInfo.Companion.defaultInfo();
         return new DeviceWithState(buildInfo, new String[]{}, null, null, null,
                 109230923452L, 22234423124L, 92340255592L, "portrait", new Date(0));
     }
 
+    /**
+     * Generates fake AppWithState
+     */
+    @NonNull
     public static AppWithState generateAppWithState() {
         return new AppWithState(generateImmutableConfig(), null, null, null,
                 null, null, null, null);
     }
 
+    /**
+     * Generates fake Configuration
+     */
+    @NonNull
     public static Configuration generateConfiguration() {
         Configuration configuration = new Configuration("5d1ec5bd39a74caa1267142706a7fb21");
         configuration.setDelivery(generateDelivery());
@@ -39,10 +49,13 @@ public class JavaHooks {
         return configuration;
     }
 
-
+    /**
+     * Generates fake Delivery
+     */
+    @NonNull
     public static Delivery generateDelivery() {
         return new Delivery() {
-            @NotNull
+            @NonNull
             @Override
             public DeliveryStatus deliver(@NonNull Report report,
                                           @NonNull DeliveryParams deliveryParams) {
@@ -58,6 +71,10 @@ public class JavaHooks {
         };
     }
 
+    /**
+     * Generates fake ImmutableConfig
+     */
+    @NonNull
     public static ImmutableConfig generateImmutableConfig() {
         return ImmutableConfigKt.convertToImmutableConfig(generateConfiguration());
     }
