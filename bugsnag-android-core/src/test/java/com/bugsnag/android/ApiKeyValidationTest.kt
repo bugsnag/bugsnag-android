3b2576988b7e5b3c93d0e0b7a92548608d7f0947
fractalwrench <fractalwrench@gmail.com>
2020-02-18 16:52:26 +0000

    feat: allow setting api key in config

--- bugsnag-android-core/src/test/java/com/bugsnag/android/ApiKeyValidationTest.kt
+++ bugsnag-android-core/src/test/java/com/bugsnag/android/ApiKeyValidationTest.kt
@@ -1,5 +1,6 @@
 package com.bugsnag.android
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ApiKeyValidationTest {
@@ -17,5 +18,31 @@ class ApiKeyValidationTest {
     @Test(expected = IllegalArgumentException::class)
     fun testNonHexApiKey() {
         Configuration("yej0492j55z92z2p")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testSettingEmptyApiKey() {
+        val config = Configuration("5d1ec5bd39a74caa1267142706a7fb21")
+        config.apiKey = ""
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testSettingWrongSizeApiKey() {
+        val config = Configuration("5d1ec5bd39a74caa1267142706a7fb21")
+        config.apiKey = "abfe05f"
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testSettingNonHexApiKey() {
+        val config = Configuration("5d1ec5bd39a74caa1267142706a7fb21")
+        config.apiKey = "yej0492j55z92z2p"
+    }
+
+    @Test
+    fun setApiKey() {
+        val config = Configuration("5d1ec5bd39a74caa1267142706a7fb21")
+        assertEquals("5d1ec5bd39a74caa1267142706a7fb21", config.apiKey)
+        config.apiKey = "000005bd39a74caa1267142706a7fb21"
+        assertEquals("000005bd39a74caa1267142706a7fb21", config.apiKey)
     }
 }
