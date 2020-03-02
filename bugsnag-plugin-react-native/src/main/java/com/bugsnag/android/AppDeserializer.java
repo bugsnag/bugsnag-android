4662519afbfcad4eeae30b03269218fad956449d
fractalwrench <fractalwrench@gmail.com>
2020-03-02 11:52:28 +0000

    feat: add deserializer for AppWithState

---
+++ bugsnag-plugin-react-native/src/main/java/com/bugsnag/android/AppDeserializer.java
@@ -0,0 +1,23 @@
+package com.bugsnag.android;
+
+import java.util.Map;
+
+class AppDeserializer implements MapDeserializer<AppWithState> {
+
+    @Override
+    public AppWithState deserialize(Map<String, Object> map) {
+        return new AppWithState(
+                MapUtils.<String>getOrNull(map, "binaryArch"),
+                MapUtils.<String>getOrNull(map, "id"),
+                MapUtils.<String>getOrNull(map, "releaseStage"),
+                MapUtils.<String>getOrNull(map, "version"),
+                MapUtils.<String>getOrNull(map, "buildUuid"),
+                MapUtils.<String>getOrNull(map, "codeBundleId"),
+                MapUtils.<String>getOrNull(map, "type"),
+                MapUtils.<Number>getOrNull(map, "versionCode"),
+                MapUtils.<Number>getOrNull(map, "duration"),
+                MapUtils.<Number>getOrNull(map, "durationInForeground"),
+                MapUtils.<Boolean>getOrNull(map, "inForeground")
+        );
+    }
+}
