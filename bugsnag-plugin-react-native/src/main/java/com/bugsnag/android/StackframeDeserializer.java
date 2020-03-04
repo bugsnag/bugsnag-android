aadce4ecadfa8d6b49ffdddca5ee333d56873fd4
fractalwrench <fractalwrench@gmail.com>
2020-03-04 13:38:46 +0000

    feat: update react-native deserialization to support code/columnNumber

--- bugsnag-plugin-react-native/src/main/java/com/bugsnag/android/StackframeDeserializer.java
+++ bugsnag-plugin-react-native/src/main/java/com/bugsnag/android/StackframeDeserializer.java
@@ -1,6 +1,5 @@
 package com.bugsnag.android;
 
-import java.util.Collections;
 import java.util.Map;
 
 class StackframeDeserializer implements MapDeserializer<Stackframe> {
@@ -11,7 +10,9 @@ class StackframeDeserializer implements MapDeserializer<Stackframe> {
                 MapUtils.<String>getOrNull(map, "method"),
                 MapUtils.<String>getOrNull(map, "file"),
                 MapUtils.<Integer>getOrNull(map, "lineNumber"),
-                MapUtils.<Boolean>getOrNull(map, "inProject")
+                MapUtils.<Boolean>getOrNull(map, "inProject"),
+                MapUtils.<Map<String, String>>getOrNull(map, "code"),
+                MapUtils.<Integer>getOrNull(map, "columnNumber")
         );
     }
 }
