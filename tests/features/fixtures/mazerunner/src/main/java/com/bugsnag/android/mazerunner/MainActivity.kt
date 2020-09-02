b4d7ea37ad286d8a17bcbb88d4ef9b0446aa00b8
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-09-02 17:41:20 +0100

    Close system dialogs

--- tests/features/fixtures/mazerunner/src/main/java/com/bugsnag/android/mazerunner/MainActivity.kt
+++ tests/features/fixtures/mazerunner/src/main/java/com/bugsnag/android/mazerunner/MainActivity.kt
@@ -11,6 +11,10 @@ import com.bugsnag.android.Configuration
 import com.bugsnag.android.EndpointConfiguration
 import com.bugsnag.android.Logger
 import com.bugsnag.android.mazerunner.scenarios.Scenario
+import android.content.Intent
+
+
+
 
 class MainActivity : Activity() {
 
@@ -19,6 +23,9 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val closeDialog = Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)
+        sendBroadcast(closeDialog)
 
         val bugsnagStarter = findViewById<Button>(R.id.startBugsnagButton)
 
