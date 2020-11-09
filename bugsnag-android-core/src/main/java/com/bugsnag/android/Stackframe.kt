1dbcba7531ee6edd2f9c070c8fcb32b52cb175f9
Karl Stenerud <kstenerud@gmail.com>
2020-11-09 13:32:09 +0100

    Put native traces into their own data objects

--- bugsnag-android-core/src/main/java/com/bugsnag/android/Stackframe.kt
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/Stackframe.kt
@@ -5,43 +5,77 @@ import java.io.IOException
 /**
  * Represents a single stackframe from a [Throwable]
  */
-class Stackframe @JvmOverloads internal constructor(
-
+class Stackframe : JsonStream.Streamable {
     /**
      * The name of the method that was being executed
      */
-    var method: String?,
+    var method: String?
+    set(value) {
+        nativeFrame?.method = value
+        field = value
+    }
 
     /**
      * The location of the source file
      */
-    var file: String?,
+    var file: String?
+        set(value) {
+            nativeFrame?.file = value
+            field = value
+        }
 
     /**
      * The line number within the source file this stackframe refers to
      */
-    var lineNumber: Number?,
+    var lineNumber: Number?
+        set(value) {
+            nativeFrame?.lineNumber = value
+            field = value
+        }
 
     /**
      * Whether the package is considered to be in your project for the purposes of grouping and
      * readability on the Bugsnag dashboard. Project package names can be set in
      * [Configuration.projectPackages]
      */
-    var inProject: Boolean?,
+    var inProject: Boolean?
 
     /**
      * Lines of the code surrounding the frame, where the lineNumber is the key (React Native only)
      */
-    var code: Map<String, String?>? = null,
+    var code: Map<String, String?>?
 
     /**
      * The column number of the frame (React Native only)
      */
-    var columnNumber: Number? = null
-) : JsonStream.Streamable {
+    var columnNumber: Number?
+
+    @JvmOverloads
+    internal constructor(method: String?, file: String?, lineNumber: Number?, inProject: Boolean?,
+        code: Map<String, String?>? = null, columnNumber: Number? = null) {
+            this.method = method
+            this.file = file
+            this.lineNumber = lineNumber
+            this.inProject = inProject
+            this.code = code
+            this.columnNumber = columnNumber
+    }
+
+    private var nativeFrame: NativeStackframe? = null
+
+    constructor(nativeFrame: NativeStackframe): this(
+        nativeFrame.method, nativeFrame.file, nativeFrame.lineNumber, false, null) {
+            this.nativeFrame = nativeFrame
+    }
 
     @Throws(IOException::class)
     override fun toStream(writer: JsonStream) {
+        val ndkFrame = nativeFrame
+        if (ndkFrame != null) {
+            ndkFrame.toStream(writer)
+            return
+        }
+
         writer.beginObject()
         writer.name("method").value(method)
         writer.name("file").value(file)
