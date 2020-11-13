859865a5ad9b610e04a57f03c11b583816c3876b
fractalwrench <fractalwrench@gmail.com>
2020-11-13 13:09:22 +0000

    test: reinstate previous packages for exceptions

--- features/fixtures/mazerunner/jvm-scenarios/src/main/java/com/bugsnag/android/mazerunner/scenarios/HandledExceptionWithoutMessage.kt
+++ features/fixtures/mazerunner/jvm-scenarios/src/main/java/com/bugsnag/android/mazerunner/scenarios/HandledExceptionWithoutMessage.kt
@@ -3,7 +3,7 @@ package com.bugsnag.android.mazerunner.scenarios
 import android.content.Context
 import com.bugsnag.android.Bugsnag
 import com.bugsnag.android.Configuration
-import com.bugsnag.android.SomeException
+import com.bugsnag.android.mazerunner.SomeException
 
 /**
  * Sends a handled exception to Bugsnag, which does not include session data.
