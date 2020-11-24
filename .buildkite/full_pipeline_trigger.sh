17ad74ce8e4886597175e1086316d26bdd810a2a
Alex Moinet <alex@bugsnag.com>
2020-11-24 20:48:41 +0000

    Tests: Add additional conditions for running full test sets

--- .buildkite/full_pipeline_trigger.sh
+++ .buildkite/full_pipeline_trigger.sh
@@ -4,7 +4,9 @@ if [[ "$BUILDKITE_MESSAGE" == *"[QUICK CI]"* ]]; then
   PIPELINE='pipeline.smoke.yml'
 elif [[ "$BUILDKITE_MESSAGE" == *"[FULL CI]"* ||
   "$BUILDKITE_BRANCH" == "next" ||
-  "$BUILDKITE_BRANCH" == "master" ]]; then
+  "$BUILDKITE_BRANCH" == "master" ||
+  "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "next" ||
+  "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "master" ]]; then
   PIPELINE='pipeline.full.yml'
 else
   PIPELINE='pipeline.smoke.yml'
