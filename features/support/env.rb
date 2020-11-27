fc679ac8a80bc4a483f0743878fd252930393c54
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-11-27 23:37:36 +0000

    Shorten wait for no requests

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,8 +2,9 @@
 # Set this explicitly
 $api_key = "a35a2a72bd230ac0aa0f52715bbdc6aa"
 
-# TODO: Remove once the Bugsnag-Integrity header has been implemented
-AfterConfiguration do |config|
+AfterConfiguration do |_config|
+  MazeRunner.config.receive_no_requests_wait = 10 if MazeRunner.config.respond_to? :receive_no_requests_wait=
+  # TODO: Remove once the Bugsnag-Integrity header has been implemented
   MazeRunner.config.enforce_bugsnag_integrity = false if MazeRunner.config.respond_to? :enforce_bugsnag_integrity=
 end
 
