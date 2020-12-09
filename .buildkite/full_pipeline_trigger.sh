69e730656acc604163b591b37f8a9be6ea9d4beb
Alex Moinet <alex@bugsnag.com>
2020-12-09 11:39:32 +0000

    Quick implementation of [integration ci] pipeline

--- .buildkite/full_pipeline_trigger.sh
+++ .buildkite/full_pipeline_trigger.sh
@@ -4,10 +4,12 @@ if [[ "$BUILDKITE_MESSAGE" == *"[quick ci]"* ]]; then
   PIPELINE='pipeline.smoke.yml'
 elif [[ "$BUILDKITE_MESSAGE" == *"[full ci]"* ||
   "$BUILDKITE_BRANCH" == "next" ||
-  "$BUILDKITE_BRANCH" == "master" ||
+  "$BUILDKITE_BRANCH" == "master" ]]; then
+  PIPELINE='pipeline.full.yml'
+elif [[ "$BUILDKITE_MESSAGE" == *"[integration ci]"* ||
   "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "next" ||
   "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "master" ]]; then
-  PIPELINE='pipeline.full.yml'
+  PIPELINE='pipeline.integration.yml'
 else
   PIPELINE='pipeline.smoke.yml'
 fi
