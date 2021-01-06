e3bd96a8f52b1ee4caac53baaf042d1aed9229bf
Alex Moinet <alex@bugsnag.com>
2021-01-06 15:36:52 +0000

    CI: Fix issue with attempting to upload pipeline yml files in a single command

--- .buildkite/pipeline_trigger.sh
+++ .buildkite/pipeline_trigger.sh
@@ -1,36 +1,37 @@
 #!/usr/bin/env sh
 
 if [[ "$BUILDKITE_MESSAGE" == *"[barebones ci]"* ]]; then
-  echo "Running barebones build due to commit message\n"
-  echo "Unit and static tests will be run\n"
-  echo "Minimal instrumentation tests will be run\n"
-  echo "End-to-end smoke tests will be run on minimum and maximum supported Android versions\n"
+  echo "Running barebones build due to commit messages"
+  echo "Unit and static tests will be run"
+  echo "Minimal instrumentation tests will be run"
+  echo "End-to-end smoke tests will be run on minimum and maximum supported Android versions"
 elif [[ "$BUILDKITE_MESSAGE" == *"[full ci]"* ||
   "$BUILDKITE_BRANCH" == "next" ||
   "$BUILDKITE_BRANCH" == "master" ]]; then
-  echo "Running full build\n"
-  echo "Unit and static tests will be run\n"
-  echo "All instrumentation tests will be run\n"
-  echo "All end-to-end tests will be run on all supported Android versions\n"
+  echo "Running full build"
+  echo "Unit and static tests will be run"
+  echo "All instrumentation tests will be run"
+  echo "All end-to-end tests will be run on all supported Android versions"
   buildkite-agent pipeline upload .buildkite/pipeline.full.yml
 elif [[ "$BUILDKITE_MESSAGE" == *"[gated-full ci]"* ||
   "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "next" ||
   "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "master" ]]; then
-  echo "Running gated-full build\n"
-  echo "Unit and static tests will be run\n"
-  echo "Minimal instrumentation tests will be run by default\n"
-  echo "End-to-end smoke tests will be run on all supported Android versions by default\n"
-  echo "If the full build is triggered this will:\n"
-  echo "  Run the instrumentation tests against all supported Android versions\n"
-  echo "  Run the full end-to-end tests on all supported Android versions\n"
-  buildkite-agent pipeline upload .buildkite/pipeline.quick.yml .buildkite/block.step.yml
+  echo "Running gated-full build"
+  echo "Unit and static tests will be run"
+  echo "Minimal instrumentation tests will be run by default"
+  echo "End-to-end smoke tests will be run on all supported Android versions by default"
+  echo "If the full build is triggered this will:"
+  echo "  Run the instrumentation tests against all supported Android versions"
+  echo "  Run the full end-to-end tests on all supported Android versions"
+  buildkite-agent pipeline upload .buildkite/pipeline.quick.yml
+  buildkite-agent pipeline upload .buildkite/block.step.yml
 elif [[ "$BUILDKITE_MESSAGE" == *"[quick ci]"* ]]; then
-  echo "Running quick build\n"
-  echo "Unit and static tests will be run\n"
-  echo "Minimal instrumentation tests will be run\n"
-  echo "End-to-end smoke tests will be run on all supported Android versions\n"
+  echo "Running quick build"
+  echo "Unit and static tests will be run"
+  echo "Minimal instrumentation tests will be run"
+  echo "End-to-end smoke tests will be run on all supported Android versions"
   buildkite-agent pipeline upload .buildkite/pipeline.quick.yml
 else
-  echo "Running quick build\n"
+  echo "Running quick build"
 fi
 
