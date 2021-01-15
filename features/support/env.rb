928ca876532d9a6d729c869bce02d1f1512cd9b2
Steve Kirkland <twometresteve@users.noreply.github.com>
2021-01-15 18:25:32 +0000

    Migrate to Maze.config

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,7 @@
 $api_key = "a35a2a72bd230ac0aa0f52715bbdc6aa"
 
 AfterConfiguration do |_config|
-  MazeRunner.config.receive_no_requests_wait = 10 if MazeRunner.config.respond_to? :receive_no_requests_wait=
+  Maze.config.receive_no_requests_wait = 10
 end
 
 Before('@skip') do |scenario|
@@ -11,37 +11,37 @@ Before('@skip') do |scenario|
 end
 
 Before('@skip_above_android_8') do |scenario|
-  skip_this_scenario("Skipping scenario") if MazeRunner.config.os_version >= 9
+  skip_this_scenario("Skipping scenario") if Maze.config.os_version >= 9
 end
 
 Before('@skip_above_android_7') do |scenario|
-  skip_this_scenario("Skipping scenario") if MazeRunner.config.os_version >= 8
+  skip_this_scenario("Skipping scenario") if Maze.config.os_version >= 8
 end
 
 Before('@skip_below_android_9') do |scenario|
-  skip_this_scenario("Skipping scenario") if MazeRunner.config.os_version < 9
+  skip_this_scenario("Skipping scenario") if Maze.config.os_version < 9
 end
 
 Before('@skip_below_android_8') do |scenario|
-  skip_this_scenario("Skipping scenario") if MazeRunner.config.os_version < 8
+  skip_this_scenario("Skipping scenario") if Maze.config.os_version < 8
 end
 
 Before('@skip_below_android_6') do |scenario|
-  skip_this_scenario("Skipping scenario") if MazeRunner.config.os_version < 6
+  skip_this_scenario("Skipping scenario") if Maze.config.os_version < 6
 end
 
 Before('@skip_below_android_5') do |scenario|
-  skip_this_scenario("Skipping scenario") if MazeRunner.config.os_version < 5
+  skip_this_scenario("Skipping scenario") if Maze.config.os_version < 5
 end
 
 Before('@skip_android_8_1') do |scenario|
-  skip_this_scenario("Skipping scenario") if MazeRunner.config.os_version == 8.1
+  skip_this_scenario("Skipping scenario") if Maze.config.os_version == 8.1
 end
 
 Before('@skip_android_10') do |scenario|
-  skip_this_scenario("Skipping scenario") if MazeRunner.config.os_version == 10
+  skip_this_scenario("Skipping scenario") if Maze.config.os_version == 10
 end
 
 Before('@skip_android_11') do |scenario|
-  skip_this_scenario("Skipping scenario") if MazeRunner.config.os_version == 11
+  skip_this_scenario("Skipping scenario") if Maze.config.os_version == 11
 end
