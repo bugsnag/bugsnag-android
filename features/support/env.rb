f2afc98617c78d3b998860d5c1a4701389764e42
Steve Kirkland <twometresteve@users.noreply.github.com>
2021-01-28 18:37:06 +0000

    Skipping scenario on Samsung devices [full ci]

--- features/support/env.rb
+++ features/support/env.rb
@@ -42,3 +42,8 @@ end
 Before('@skip_android_11') do |scenario|
   skip_this_scenario("Skipping scenario") if Maze.config.os_version == 11
 end
+
+Before('@skip_samsung') do |scenario|
+  skip_this_scenario("Skipping scenario") if Maze.driver.capabilities['device']&.downcase&.include? 'samsung'
+end
+
