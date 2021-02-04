6055339c45cbe6c8b1a392a5ff922caa02aadced
fractalwrench <fractalwrench@gmail.com>
2021-02-04 09:41:25 +0000

    remove unused import

--- bugsnag-android-core/src/main/java/com/bugsnag/android/DeliveryDelegate.java
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/DeliveryDelegate.java
@@ -7,7 +7,6 @@ import androidx.annotation.VisibleForTesting;
 
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.RejectedExecutionException;
