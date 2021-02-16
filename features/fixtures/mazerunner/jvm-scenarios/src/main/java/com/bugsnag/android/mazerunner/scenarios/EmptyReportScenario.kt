8ff6f8ec2b025c06c07f0e66e648ad06b8c22d76
Steve Kirkland <twometresteve@users.noreply.github.com>
2021-02-16 10:27:52 +0000

    Reinstate scenario code

--- features/fixtures/mazerunner/jvm-scenarios/src/main/java/com/bugsnag/android/mazerunner/scenarios/EmptyReportScenario.kt
+++ features/fixtures/mazerunner/jvm-scenarios/src/main/java/com/bugsnag/android/mazerunner/scenarios/EmptyReportScenario.kt
@@ -3,6 +3,7 @@ package com.bugsnag.android.mazerunner.scenarios
 import android.content.Context
 import com.bugsnag.android.Bugsnag
 import com.bugsnag.android.Configuration
+import java.io.File
 
 internal class EmptyReportScenario(
     config: Configuration,
@@ -11,9 +12,16 @@ internal class EmptyReportScenario(
 ) : Scenario(config, context, eventMetadata) {
 
     override fun startBugsnag(startBugsnagOnly: Boolean) {
+        config.autoTrackSessions = false
+
         if (startBugsnagOnly) {
             disableAllDelivery(config)
+        } else {
+            val errDir = File(context.cacheDir, "bugsnag-errors")
+            val files = errDir.listFiles()
+            files.forEach { it.writeText("") }
         }
+        super.startBugsnag(startBugsnagOnly)
     }
 
     override fun startScenario() {
