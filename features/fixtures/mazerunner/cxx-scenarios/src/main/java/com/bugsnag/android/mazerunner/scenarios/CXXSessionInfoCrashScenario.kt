5d8ee4ffa86df8595ab497f12f4bc1d6769bb221
fractalwrench <fractalwrench@gmail.com>
2021-02-16 16:15:13 +0000

    test: remove sleep() and flush() from E2E scenarios

---
+++ features/fixtures/mazerunner/cxx-scenarios/src/main/java/com/bugsnag/android/mazerunner/scenarios/CXXSessionInfoCrashScenario.kt
@@ -0,0 +1,29 @@
+package com.bugsnag.android.mazerunner.scenarios
+
+import android.content.Context
+import com.bugsnag.android.Bugsnag
+import com.bugsnag.android.Configuration
+
+class CXXSessionInfoCrashScenario(
+    config: Configuration,
+    context: Context,
+    eventMetadata: String?
+) : Scenario(config, context, eventMetadata) {
+
+    init {
+        System.loadLibrary("cxx-scenarios")
+        config.autoTrackSessions = false
+    }
+
+    external fun crash(value: Int): Int
+
+    override fun startScenario() {
+        super.startScenario()
+        if (eventMetadata!! != "non-crashy") {
+            Bugsnag.startSession()
+            Bugsnag.notify(Exception("For the first"))
+            Bugsnag.notify(Exception("For the second"))
+            crash(3837)
+        }
+    }
+}
---
+++ features/fixtures/mazerunner/cxx-scenarios/src/main/java/com/bugsnag/android/mazerunner/scenarios/CXXStartSessionScenario.kt
@@ -0,0 +1,27 @@
+package com.bugsnag.android.mazerunner.scenarios
+
+import android.content.Context
+import com.bugsnag.android.Bugsnag
+import com.bugsnag.android.Configuration
+
+class CXXStartSessionScenario(
+    config: Configuration,
+    context: Context,
+    eventMetadata: String?
+) : Scenario(config, context, eventMetadata) {
+
+    init {
+        System.loadLibrary("cxx-scenarios")
+        config.autoTrackSessions = false
+    }
+
+    external fun crash(counter: Int): Int
+
+    override fun startScenario() {
+        super.startScenario()
+        if (eventMetadata!! != "non-crashy") {
+            Bugsnag.startSession()
+            crash(0)
+        }
+    }
+}
