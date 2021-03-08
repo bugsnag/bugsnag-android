3b5c7f505cacb2582423ebe292c5a8226970ae4c
fractalwrench <fractalwrench@gmail.com>
2021-03-08 14:33:16 +0000

    [full ci] test: fix flaky session scenario

--- features/fixtures/mazerunner/jvm-scenarios/src/main/java/com/bugsnag/android/mazerunner/scenarios/ManualSessionSmokeScenario.kt
+++ features/fixtures/mazerunner/jvm-scenarios/src/main/java/com/bugsnag/android/mazerunner/scenarios/ManualSessionSmokeScenario.kt
@@ -3,8 +3,6 @@ package com.bugsnag.android.mazerunner.scenarios
 import android.content.Context
 import com.bugsnag.android.Bugsnag
 import com.bugsnag.android.Configuration
-import com.bugsnag.android.createDefaultDelivery
-import com.bugsnag.android.mazerunner.InterceptingDelivery
 
 /**
  * Sends an exception after pausing the session
@@ -17,31 +15,25 @@ internal class ManualSessionSmokeScenario(
 
     override fun startBugsnag(startBugsnagOnly: Boolean) {
         config.autoTrackSessions = false
-
-        if (!startBugsnagOnly) {
-            val baseDelivery = createDefaultDelivery()
-            var state = 0
-            config.delivery = InterceptingDelivery(baseDelivery) {
-                when (state) {
-                    0 -> Bugsnag.notify(generateException())
-                    1 -> {
-                        Bugsnag.pauseSession()
-                        Bugsnag.notify(generateException())
-                    }
-                    2 -> {
-                        Bugsnag.resumeSession()
-                        throw generateException()
-                    }
-                }
-                state++
-            }
-        }
         super.startBugsnag(startBugsnagOnly)
     }
 
     override fun startScenario() {
         super.startScenario()
         Bugsnag.setUser("123", "ABC.CBA.CA", "ManualSessionSmokeScenario")
+
+        // send session
         Bugsnag.startSession()
+
+        // send exception with session
+        Bugsnag.notify(generateException())
+
+        // send exception without session
+        Bugsnag.pauseSession()
+        Bugsnag.notify(generateException())
+
+        // throw exception with session
+        Bugsnag.resumeSession()
+        throw generateException()
     }
 }
