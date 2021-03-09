f38b4affb529eb4cb015d0588c8712d07f12259c
Karl Stenerud <kstenerud@gmail.com>
2021-03-09 15:16:29 +0100

    Backport ANR fixes to unity branch

--- bugsnag-plugin-android-anr/src/main/jni/anr_handler.c
+++ bugsnag-plugin-android-anr/src/main/jni/anr_handler.c
@@ -1,119 +1,224 @@
 #include "anr_handler.h"
+#include <android/log.h>
 #include <errno.h>
+#include <jni.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include <signal.h>
 #include <string.h>
-#include <android/log.h>
-#include <jni.h>
+#include <unistd.h>
 
-#include "utils/string.h"
-
-static pthread_t bsg_watchdog_thread;
-static sigset_t bsg_anr_sigmask;
+#include "anr_google.h"
 
 // Lock for changing the handler configuration
 static pthread_mutex_t bsg_anr_handler_config = PTHREAD_MUTEX_INITIALIZER;
+
 // A proxy for install/uninstall state, to avoid needing to unset the handler
 // on the sigquit-watching thread
 static bool enabled = false;
 static bool installed = false;
-static bool invokePrevHandler = true;
+
+static pthread_t watchdog_thread;
+static bool should_wait_for_semaphore = false;
+static sem_t reporter_thread_semaphore;
+static volatile bool should_report_anr = false;
+static struct sigaction original_sigquit_handler;
 
 static JavaVM *bsg_jvm = NULL;
 static jmethodID mthd_notify_anr_detected = NULL;
 static jobject obj_plugin = NULL;
 
-/* The previous SIGQUIT handler */
-struct sigaction bsg_sigquit_sigaction_previous;
-
-
-void bsg_notify_anr_detected(JNIEnv *env) {
-  (*env)->CallVoidMethod(env, obj_plugin, mthd_notify_anr_detected);
+// duplication required for JNI methods that originally came from
+// bugsnag-plugin-android-ndk. Until a shared C module is available
+// for sharing common code when PLAT-5794 is addressed, this
+// duplication is a necessary evil.
+bool anr_bsg_check_and_clear_exc(JNIEnv *env) {
+  if (env == NULL) {
+    return false;
+  }
+  if ((*env)->ExceptionCheck(env)) {
+    (*env)->ExceptionClear(env);
+    return true;
+  }
+  return false;
 }
 
-bool bsg_configure_anr_jni(JNIEnv *env) {
-  // get a global reference to the AnrPlugin class
+jclass anr_bsg_safe_find_class(JNIEnv *env, const char *clz_name) {
+  if (env == NULL) {
+    return NULL;
+  }
+  if (clz_name == NULL) {
+    return NULL;
+  }
+  jclass clz = (*env)->FindClass(env, clz_name);
+  anr_bsg_check_and_clear_exc(env);
+  return clz;
+}
+
+jmethodID anr_bsg_safe_get_method_id(JNIEnv *env, jclass clz, const char *name,
+                                     const char *sig) {
+  if (env == NULL || clz == NULL || name == NULL || sig == NULL) {
+    return NULL;
+  }
+  jmethodID methodId = (*env)->GetMethodID(env, clz, name, sig);
+  anr_bsg_check_and_clear_exc(env);
+  return methodId;
+}
+
+bool configure_anr_jni(
+        JNIEnv *env) { // get a global reference to the AnrPlugin class
   // https://developer.android.com/training/articles/perf-jni#faq:-why-didnt-findclass-find-my-class
+  if (env == NULL) {
+    return false;
+  }
   int result = (*env)->GetJavaVM(env, &bsg_jvm);
   if (result != 0) {
-    // Failed to fetch VM, cannot continue
     return false;
   }
 
-  jclass clz = (*env)->FindClass(env, "com/bugsnag/android/AnrPlugin");
-  mthd_notify_anr_detected = (*env)->GetMethodID(env, clz, "notifyAnrDetected", "()V");
+  jclass clz = anr_bsg_safe_find_class(env, "com/bugsnag/android/AnrPlugin");
+  if (anr_bsg_check_and_clear_exc(env) || clz == NULL) {
+    return false;
+  }
+  mthd_notify_anr_detected = anr_bsg_safe_get_method_id(
+          env, clz, "notifyAnrDetected", "()V");
   return true;
 }
 
-void bsg_handle_sigquit(int signum, siginfo_t *info, void *user_context) {
-  if (enabled) {
-    // invoke a JNI call from the SIGQUIT handler
-    JNIEnv *env;
-    int result = (*bsg_jvm)->GetEnv(bsg_jvm, (void **)&env, JNI_VERSION_1_4);
-
-    if (result == JNI_OK) { // already attached
-      bsg_notify_anr_detected(env);
-    } else if (result == JNI_EDETACHED) { // attach before calling JNI
-      if ((*bsg_jvm)->AttachCurrentThread(bsg_jvm, &env, NULL) == 0) {
-        bsg_notify_anr_detected(env);
-        (*bsg_jvm)->DetachCurrentThread(bsg_jvm); // detach to restore initial condition
-      }
-    } // All other results are error codes
+static void notify_anr_detected() {
+  if (!enabled) {
+    return;
   }
 
-  // Invoke previous handler, if a custom handler has been set
-  // This can be conditionally switched off on certain platforms (e.g. Unity) where
-  // this behaviour is not desirable due to Unity's implementation failing with a SIGSEGV
-  if (invokePrevHandler) {
-    struct sigaction previous = bsg_sigquit_sigaction_previous;
-    if (previous.sa_flags & SA_SIGINFO) {
-      previous.sa_sigaction(SIGQUIT, info, user_context);
-    } else if (previous.sa_handler == SIG_DFL) {
-      // Do nothing, the default action is nothing
-    } else if (previous.sa_handler != SIG_IGN) {
-      void (*previous_handler)(int) = previous.sa_handler;
-      previous_handler(signum);
-    }
+  bool should_detach = false;
+  JNIEnv *env;
+  int result = (*bsg_jvm)->GetEnv(bsg_jvm, (void **)&env, JNI_VERSION_1_4);
+  switch (result) {
+    case JNI_OK:
+      break;
+    case JNI_EDETACHED:
+      result = (*bsg_jvm)->AttachCurrentThread(bsg_jvm, &env, NULL);
+      if (result != 0) {
+        BUGSNAG_LOG("Failed to call JNIEnv->AttachCurrentThread(): %d", result);
+        return;
+      }
+      should_detach = true;
+      break;
+    default:
+      BUGSNAG_LOG("Failed to call JNIEnv->GetEnv(): %d", result);
+      return;
+  }
+
+  if (obj_plugin != NULL && mthd_notify_anr_detected != NULL) {
+    (*env)->CallVoidMethod(env, obj_plugin, mthd_notify_anr_detected);
+    anr_bsg_check_and_clear_exc(env);
+  }
+
+  if (should_detach) {
+    (*bsg_jvm)->DetachCurrentThread(
+            bsg_jvm); // detach to restore initial condition
   }
 }
 
-/**
- * Configure ANR detection using a signal handler listening for SIGQUIT
- */
-void *bsg_monitor_anrs(void *_arg) {
-  struct sigaction handler;
-  sigemptyset(&handler.sa_mask);
-  handler.sa_sigaction = bsg_handle_sigquit;
+static void *sigquit_watchdog_thread_main(void *_) {
+  static const useconds_t delay_2sec = 2000000;
+  static const useconds_t delay_100ms = 100000;
+  static const useconds_t delay_10ms = 10000;
 
-  // remove SA_ONSTACK flag as we don't require information about the SIGQUIT signal.
-  // specifying this flag results in a crash when performing a JNI call. For further context
-  // see https://issuetracker.google.com/issues/37035211
-  handler.sa_flags = SA_SIGINFO;
-  int success = sigaction(SIGQUIT, &handler, &bsg_sigquit_sigaction_previous);
-  if (success != 0) {
-    BUGSNAG_LOG("Failed to install SIGQUIT handler: %s", strerror(errno));
+  // Wait until our SIGQUIT handler is ready for us to start.
+  // Use sem_wait if possible, falling back to polling.
+  if (!should_wait_for_semaphore || sem_wait(&reporter_thread_semaphore) != 0) {
+    while (!should_report_anr) {
+      usleep(delay_100ms);
+    }
   }
 
+  // Force at least one task switch after being triggered, ensuring that the
+  // signal masks are properly settled before triggering the Google handler.
+  usleep(delay_10ms);
+
+  // Do our ANR processing
+  notify_anr_detected();
+
+  // Trigger Google ANR processing
+  bsg_google_anr_call();
+
+  // Give a little time for the Google handler to dump state, then exit this
+  // thread.
+  usleep(delay_2sec);
   return NULL;
 }
 
-bool bsg_handler_install_anr(JNIEnv *env, jobject plugin, jboolean callPreviousSigquitHandler) {
+static void handle_sigquit(int signum, siginfo_t *info, void *user_context) {
+  // Re-block SIGQUIT so that the Google handler can trigger
+  sigset_t sigmask;
+  sigemptyset(&sigmask);
+  sigaddset(&sigmask, SIGQUIT);
+  pthread_sigmask(SIG_BLOCK, &sigmask, NULL);
+  sigaction(SIGQUIT, &original_sigquit_handler, NULL);
+
+  // Instruct our watchdog thread to report the ANR and also call Google
+  should_report_anr = true;
+  // Although sem_post is not officially marked as async-safe, the Android
+  // implementation simply does an atomic compare-and-exchange when there is
+  // only one thread waiting (which is the case here).
+  // https://cs.android.com/android/platform/superproject/+/master:bionic/libc/bionic/semaphore.cpp;l=289?q=sem_post&ss=android
+  if (sem_post(&reporter_thread_semaphore) != 0) {
+    // The only possible failure from sem_post is EOVERFLOW, which won't happen
+    // in this code. But implementations can change...
+    BUGSNAG_LOG("Could not unlock semaphore");
+  }
+}
+
+static void install_signal_handler() {
+  if (!bsg_google_anr_init()) {
+    BUGSNAG_LOG("Failed to initialize Google ANR caller. ANRs won't be sent to "
+                "Google.");
+    // We can still report to Bugsnag, so continue.
+  }
+
+  if (sem_init(&reporter_thread_semaphore, 0, 0) == 0) {
+    should_wait_for_semaphore = true;
+  } else {
+    BUGSNAG_LOG("Failed to init semaphore");
+    // We can still poll should_report_anr, so continue.
+  }
+
+  // Start the watchdog thread
+  if (pthread_create(&watchdog_thread, NULL, sigquit_watchdog_thread_main,
+                     NULL) != 0) {
+    BUGSNAG_LOG(
+            "Could not create ANR watchdog thread. ANRs won't be sent to Bugsnag.");
+    return;
+  }
+
+  // Install our signal handler
+  struct sigaction handler;
+  sigemptyset(&handler.sa_mask);
+  handler.sa_sigaction = handle_sigquit;
+  handler.sa_flags = SA_SIGINFO;
+  if (sigaction(SIGQUIT, &handler, &original_sigquit_handler) != 0) {
+    BUGSNAG_LOG(
+            "Failed to install SIGQUIT handler: %s. ANRs won't be sent to Bugsnag.",
+            strerror(errno));
+    return;
+  }
+
+  // Unblock SIGQUIT so that our handler will be called
+  sigset_t anr_sigmask;
+  sigemptyset(&anr_sigmask);
+  sigaddset(&anr_sigmask, SIGQUIT);
+  if (pthread_sigmask(SIG_UNBLOCK, &anr_sigmask, NULL) != 0) {
+    BUGSNAG_LOG("Could not unblock SIGQUIT. ANRs won't be sent to Bugsnag.");
+  }
+}
+
+bool bsg_handler_install_anr(JNIEnv *env, jobject plugin, jboolean ignore_callPreviousSigquitHandler) {
   pthread_mutex_lock(&bsg_anr_handler_config);
-  invokePrevHandler = callPreviousSigquitHandler;
 
-  if (!installed && bsg_configure_anr_jni(env)) {
+  if (!installed && configure_anr_jni(env) && plugin != NULL) {
     obj_plugin = (*env)->NewGlobalRef(env, plugin);
-    sigemptyset(&bsg_anr_sigmask);
-    sigaddset(&bsg_anr_sigmask, SIGQUIT);
-
-    int mask_status = pthread_sigmask(SIG_BLOCK, &bsg_anr_sigmask, NULL);
-    if (mask_status != 0) {
-      BUGSNAG_LOG("Failed to mask SIGQUIT: %s", strerror(mask_status));
-    } else {
-      pthread_create(&bsg_watchdog_thread, NULL, bsg_monitor_anrs, NULL);
-      // unblock the current thread
-      pthread_sigmask(SIG_UNBLOCK, &bsg_anr_sigmask, NULL);
-    }
+    install_signal_handler();
     installed = true;
   }
   enabled = true;
