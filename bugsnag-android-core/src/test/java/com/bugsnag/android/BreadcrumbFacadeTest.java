1c352255884411414d4fba0d4f62818f8f3c9f13
fractalwrench <fractalwrench@gmail.com>
2021-04-30 11:30:51 +0100

    test: add test for breadcrumb timestamp

--- bugsnag-android-core/src/test/java/com/bugsnag/android/BreadcrumbFacadeTest.java
+++ bugsnag-android-core/src/test/java/com/bugsnag/android/BreadcrumbFacadeTest.java
@@ -75,4 +75,9 @@ public class BreadcrumbFacadeTest {
     public void dateValid() {
         assertEquals(new Date(0).getTime(), crumb.getTimestamp().getTime());
     }
+
+    @Test
+    public void stringDateValid() {
+        assertEquals("1970-01-01T00:00:00.000Z", crumb.getStringTimestamp());
+    }
 }
