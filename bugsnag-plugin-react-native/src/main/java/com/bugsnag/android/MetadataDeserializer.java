a602579e5008e81b67596a3c8155163fa90c14f7
fractalwrench <fractalwrench@gmail.com>
2021-06-17 14:55:21 +0100

    perf: optimize metadata implementation

--- bugsnag-plugin-react-native/src/main/java/com/bugsnag/android/MetadataDeserializer.java
+++ bugsnag-plugin-react-native/src/main/java/com/bugsnag/android/MetadataDeserializer.java
@@ -6,7 +6,11 @@ import java.util.concurrent.ConcurrentHashMap;
 class MetadataDeserializer implements MapDeserializer<Metadata> {
     @Override
     public Metadata deserialize(Map<String, Object> map) {
-        ConcurrentHashMap<String, Object> store = new ConcurrentHashMap<>(map);
+        // cast map to retain original signature until next major version bump, as this
+        // method signature is used by Unity/React native
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        Map<String, Map<String, Object>> data = (Map) map;
+        ConcurrentHashMap<String, Map<String, Object>> store = new ConcurrentHashMap<>(data);
         return new Metadata(store);
     }
 }
