475a5b722bfe660bb9c5b1d799caf6c4fa9afdd4
fractalwrench <fractalwrench@gmail.com>
2021-07-14 16:13:37 +0100

    test: update test code

--- bugsnag-plugin-android-ndk/src/androidTest/java/com/bugsnag/android/ndk/VerifyUtils.kt
+++ bugsnag-plugin-android-ndk/src/androidTest/java/com/bugsnag/android/ndk/VerifyUtils.kt
@@ -4,8 +4,10 @@ import java.lang.IllegalStateException
 
 internal fun verifyNativeRun(code: Int): Boolean {
     if (code != 0) {
-        throw IllegalStateException("Native test suite failed." +
-                " Filter the device logs by 'BugsnagNDKTest' to get the results.")
+        throw IllegalStateException(
+            "Native test suite failed." +
+                " Filter the device logs by 'BugsnagNDKTest' to get the results."
+        )
     } else {
         return true
     }
