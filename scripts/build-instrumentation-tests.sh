94eb49e53b49ec4eb6f9bb657bff9bea9cbdab66
Steve Kirkland-Walton <steve@bugsnag.com>
2021-10-25 18:30:09 +0100

    Build example app for Espresso tests [full ci]

--- scripts/build-instrumentation-tests.sh
+++ scripts/build-instrumentation-tests.sh
@@ -1,2 +1,7 @@
-# Build the Espresso Test App
+  # Build the example app as the Espresso "target" (which is not actually used)
+pushd examples/sdk-app-example
+  ./gradlew clean assembleRelease
+popd
+
+# Build the test app
 ./gradlew assembleAndroidTest --stacktrace
