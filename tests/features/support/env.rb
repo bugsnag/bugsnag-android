c64721a62a8aa64d65ceff6d113d6c7c4c56ce0c
Steve Kirkland-Walton <steve@bugsnag.com>
2021-11-28 23:40:39 +0000

    CI corrections

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -5,17 +5,17 @@ MazeRunner.config.enforce_bugsnag_integrity = false
 MazeRunner.config.receive_requests_wait = 60
 
 Before('@skip_android_9') do |scenario|
-  skip_this_scenario("Skipping scenario") if Maze.config.os_version = 9
+  skip_this_scenario("Skipping scenario") if MazeRunner.config.os_version = 9
 end
 
 Before('@skip_below_android_9') do |scenario|
-  skip_this_scenario("Skipping scenario") if Maze.config.os_version < 9
+  skip_this_scenario("Skipping scenario") if MazeRunner.config.os_version < 9
 end
 
 Before('@skip_below_android_8') do |scenario|
-  skip_this_scenario("Skipping scenario") if Maze.config.os_version < 8
+  skip_this_scenario("Skipping scenario") if MazeRunner.config.os_version < 8
 end
 
 Before('@skip_above_android_7') do |scenario|
-  skip_this_scenario("Skipping scenario") if Maze.config.os_version > 7
+  skip_this_scenario("Skipping scenario") if MazeRunner.config.os_version > 7
 end
