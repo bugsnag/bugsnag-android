5978ca6ac61c4f5d6abf1b67ec16b527a99507a8
Karl Stenerud <kstenerud@gmail.com>
2022-01-04 14:10:13 +0100

    Harmonize all string copying in the NDK layer

--- bugsnag-plugin-android-ndk/src/main/jni/utils/string.c
+++ bugsnag-plugin-android-ndk/src/main/jni/utils/string.c
@@ -3,41 +3,20 @@
 #include <stdbool.h>
 #include <string.h>
 
-void bsg_strcpy(char *dst, const char *src) { bsg_strncpy(dst, src, INT_MAX); }
-
-void bsg_strncpy(char *dst, const char *src, size_t len) {
-  int i = 0;
-  while (i <= len) {
-    char current = src[i];
-    dst[i] = current;
-    if (current == '\0') {
-      break;
-    }
-    i++;
-  }
-}
+// Anything more than this and we shouldn't even be sending or using it.
+const size_t STRING_MAX_LENGTH = 1024 * 1024 * 10;
 
 size_t bsg_strlen(const char *str) {
   if (str == NULL) {
     return 0;
   }
-  size_t i = 0;
-  while (true) {
-    char current = str[i];
-    if (current == '\0') {
-      return i;
-    } else if (i == INT_MAX) {
-      return INT_MAX;
-    }
-    i++;
-  }
+  return strnlen(str, STRING_MAX_LENGTH);
 }
 
-void bsg_strncpy_safe(char *dst, const char *src, int dst_size) {
-  if (dst_size == 0)
+void bsg_strncpy(char *dst, const char *src, size_t dst_size) {
+  if (src == NULL || dst == NULL || dst_size == 0) {
     return;
-  dst[0] = '\0';
-  if (src != NULL) {
-    strncat(dst, src, dst_size - 1);
   }
+  dst[0] = '\0';
+  strncat(dst, src, dst_size - 1);
 }
