6ebcfc2c5ab728cb57a2fedbc59e96b203045153
Jason <jason.morris@bugsnag.com>
2022-01-31 13:32:26 +0000

    v5.19.2

--- bugsnag-android-core/src/main/java/com/bugsnag/android/Notifier.kt
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/Notifier.kt
@@ -7,7 +7,7 @@ import java.io.IOException
  */
 class Notifier @JvmOverloads constructor(
     var name: String = "Android Bugsnag Notifier",
-    var version: String = "5.19.1",
+    var version: String = "5.19.2",
     var url: String = "https://bugsnag.com"
 ) : JsonStream.Streamable {
 
