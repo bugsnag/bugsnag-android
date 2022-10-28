bc0102d163b2eb66616ce8ff98d8340cdaf07e85
Steve Kirkland-Walton <steve@bugsnag.com>
2022-10-28 22:08:05 +0100

    Lint error

--- features/fixtures/mazerunner/cxx-scenarios-bugsnag/src/main/java/com/bugsnag/android/mazerunner/scenarios/CXXBackgroundNotifyScenario.kt
+++ features/fixtures/mazerunner/cxx-scenarios-bugsnag/src/main/java/com/bugsnag/android/mazerunner/scenarios/CXXBackgroundNotifyScenario.kt
@@ -6,7 +6,6 @@ import android.os.Handler
 import android.os.Looper
 import com.bugsnag.android.Configuration
 import java.util.concurrent.atomic.AtomicBoolean
-import kotlin.concurrent.thread
 
 internal class CXXBackgroundNotifyScenario(
     config: Configuration,
