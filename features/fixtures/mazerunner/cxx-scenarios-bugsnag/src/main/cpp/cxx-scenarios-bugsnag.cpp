10d575ece0e687bb64d32223832171517bcd80d6
Jason <jason.morris@bugsnag.com>
2022-11-10 11:39:22 +0000

    fix(ndk): end2end test for calling refreshSymbolTable during a crash

--- features/fixtures/mazerunner/cxx-scenarios-bugsnag/src/main/cpp/cxx-scenarios-bugsnag.cpp
+++ features/fixtures/mazerunner/cxx-scenarios-bugsnag/src/main/cpp/cxx-scenarios-bugsnag.cpp
@@ -404,4 +404,19 @@ Java_com_bugsnag_android_mazerunner_scenarios_MetadataStringsTooLargeScenario_na
     return value / x / 8;
 }
 
+static volatile int *the_value;
+
+int __attribute__((optnone)) get_the_null_value() {
+  // assume this function is very interesting
+  return *the_value;
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_bugsnag_android_mazerunner_scenarios_CXXRefreshSymbolTableDuringCrashScenario_activate(
+    JNIEnv *env, jobject thiz) {
+
+  return get_the_null_value();
+}
+
 }
