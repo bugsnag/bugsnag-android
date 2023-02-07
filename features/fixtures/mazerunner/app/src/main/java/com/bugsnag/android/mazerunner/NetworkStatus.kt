1d0c903948d0efcbd1d62671fa1c6c30407a5be6
Jason <jason.morris@bugsnag.com>
2023-02-07 13:48:16 +0000

    test(mazerunner): check ConnectivityManager for network status (where possible)

---
+++ features/fixtures/mazerunner/app/src/main/java/com/bugsnag/android/mazerunner/NetworkStatus.kt
@@ -0,0 +1,43 @@
+package com.bugsnag.android.mazerunner
+
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import java.net.URL
+
+enum class NetworkStatus {
+    NO_NETWORK,
+    UNKNOWN_CAPABILITIES,
+    NO_INTERNET,
+    CONNECTED,
+    DISCONNECTED
+}
+
+val Context.networkStatus: NetworkStatus
+    get() {
+        val connectivityManager = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return NetworkStatus.NO_NETWORK
+            val capabilities = connectivityManager.getNetworkCapabilities(network)
+                ?: return NetworkStatus.UNKNOWN_CAPABILITIES
+
+            return if (capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)) NetworkStatus.CONNECTED
+            else NetworkStatus.NO_INTERNET
+        } else {
+            val networkInfo =
+                connectivityManager.activeNetworkInfo ?: return NetworkStatus.NO_NETWORK
+
+            if (networkInfo.isAvailable && networkInfo.isConnected) {
+                return try {
+                    URL("https://www.google.com").readText()
+                    NetworkStatus.CONNECTED
+                } catch (e: Exception) {
+                    NetworkStatus.NO_INTERNET
+                }
+            }
+
+            return NetworkStatus.DISCONNECTED
+        }
+    }
