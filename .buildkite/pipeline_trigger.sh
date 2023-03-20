a8222079393086f526bb99e46713c3b31cfd174f
Steve Kirkland-Walton <steve@bugsnag.com>
2023-03-20 17:05:09 +0000

    Adjust BitBar concurrency settings

--- .buildkite/pipeline_trigger.sh
+++ .buildkite/pipeline_trigger.sh
@@ -11,9 +11,3 @@ else
   # Basic build, but allow a full build to be triggered
   buildkite-agent pipeline upload .buildkite/block.step.yml
 fi
-
-# Run extra BitBar steps if instructed to
-if [[ "$BUILDKITE_MESSAGE" == *"[bb]"* ||
-      "$DEVICE_FARM" == *"BB"* ]]; then
-  buildkite-agent pipeline upload .buildkite/pipeline.bb.yml
-fi
