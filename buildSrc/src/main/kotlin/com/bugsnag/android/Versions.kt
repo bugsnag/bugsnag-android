86fa8b1cb4539515bd46731813fd9cd0dd60dab7
jason <lemnik@users.noreply.github.com>
2023-09-06 11:17:09 +0100

    dep(benchmark): androidx.benchmark version 1.1.1

--- buildSrc/src/main/kotlin/com/bugsnag/android/Versions.kt
+++ buildSrc/src/main/kotlin/com/bugsnag/android/Versions.kt
@@ -18,7 +18,7 @@ object Versions {
     val detektPlugin = "1.23.1"
     val ktlintPlugin = "10.2.0"
     val dokkaPlugin = "1.9.0"
-    val benchmarkPlugin = "1.0.0"
+    val benchmarkPlugin = "1.1.1"
 
     // dependencies
     val supportLib = "1.1.0"
