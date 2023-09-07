4ddae82e0f02f6a61336cbc5e0b6edbd254c4602
yingying.chen <yingying.chen@smartbear.com>
2023-09-07 14:45:05 +0100

    feature(exitInfo) convert tombstone file to bugsnagthread tests

--- bugsnag-plugin-android-exitinfo/src/test/java/com/bugsnag/android/TombstoneParserTest.kt
+++ bugsnag-plugin-android-exitinfo/src/test/java/com/bugsnag/android/TombstoneParserTest.kt
@@ -1,6 +1,7 @@
 package com.bugsnag.android
 
 import android.app.ApplicationExitInfo
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
@@ -22,21 +23,43 @@ internal class TombstoneParserTest {
     @Test
     fun parseValidInputStream() {
         `when`(exitInfo.traceInputStream).thenReturn(file)
-        TombstoneParser(logger).parse(exitInfo)
+        val threads = mutableListOf<Thread>()
+        TombstoneParser(logger).parse(exitInfo){
+            threads.add(it)
+        }
+
+        assertEquals("30640", threads.first().id )
+        assertEquals("30639", threads.last().id )
+        assertEquals(4, threads.first().stacktrace.size)
+        assertEquals("__rt_sigtimedwait", threads.first().stacktrace.first().method )
+        assertEquals("/apex/com.android.runtime/lib64/bionic/libc.so", threads.first().stacktrace.first().file )
+        assertEquals(667096L, threads.first().stacktrace.first().lineNumber )
+        assertEquals(8L, threads.first().stacktrace.first().symbolAddress )
+        assertEquals(0L, threads.first().stacktrace.first().loadAddress )
+        assertEquals("01331f74b0bb2cb958bdc15282b8ec7b", threads.first().stacktrace.first().codeIdentifier )
     }
 
     @Test
     fun parseNullInputStream() {
         `when`(exitInfo.traceInputStream).thenReturn(null)
-        TombstoneParser(logger).parse(exitInfo)
+        val threads = mutableListOf<Thread>()
+        TombstoneParser(logger).parse(exitInfo){
+            threads.add(it)
+        }
         verify(exitInfo, times(1)).traceInputStream
+        assertEquals(0, threads.size)
     }
 
     @Test
     fun parseInvalidInputStream() {
         val junkData = ByteArray(128) { it.toByte() }
         `when`(exitInfo.traceInputStream).thenReturn(junkData.inputStream())
-        TombstoneParser(logger).parse(exitInfo)
+        val threads = mutableListOf<Thread>()
+        TombstoneParser(logger).parse(exitInfo){
+            threads.add(it)
+        }
+
         verify(exitInfo, times(1)).traceInputStream
+        assertEquals(0, threads.size)
     }
 }
