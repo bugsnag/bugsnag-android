ca6ec195786a6997be011c3af9e7e61b63050308
yingying.chen <yingying.chen@smartbear.com>
2023-09-07 10:17:40 +0100

    feature(exitInfo) convert tombstone file to bugsnagthread

--- bugsnag-plugin-android-exitinfo/src/main/java/com/bugsnag/android/TombstoneParser.kt
+++ bugsnag-plugin-android-exitinfo/src/main/java/com/bugsnag/android/TombstoneParser.kt
@@ -3,20 +3,53 @@ package com.bugsnag.android
 import android.app.ApplicationExitInfo
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.bugsnag.android.repackaged.server.os.TombstoneProtos
 import com.bugsnag.android.repackaged.server.os.TombstoneProtos.Tombstone
+import com.bugsnag.android.Thread as BugsnagThread
+import com.bugsnag.android.Thread.State
 
 internal class TombstoneParser(
     private val logger: Logger
 ) {
+
     @RequiresApi(Build.VERSION_CODES.R)
-    fun parse(exitInfo: ApplicationExitInfo) {
+    fun parse(
+        exitInfo: ApplicationExitInfo,
+        threadConsumer: (BugsnagThread) -> Unit
+    ) {
         try {
             val trace: Tombstone = exitInfo.traceInputStream?.use {
                 Tombstone.newBuilder().mergeFrom(it).build()
             } ?: return
-            trace.threadsMap.values
+            convertToBugsnagThread(trace.threadsMap.values, threadConsumer)
         } catch (ex: Throwable) {
             logger.w("Tombstone input stream threw an Exception", ex)
+        }
+    }
+
+    private fun convertToBugsnagThread(
+        values: MutableCollection<TombstoneProtos.Thread>,
+        threadConsumer: (BugsnagThread) -> Unit
+    ) {
+        values.forEach { thread ->
+            val stackFrames = thread.currentBacktraceList.map {
+                val stackFrame = Stackframe(it.functionName, it.fileName, it.relPc, null)
+                stackFrame.symbolAddress = it.functionOffset
+                stackFrame.loadAddress = it.fileMapOffset
+                stackFrame.codeIdentifier = it.buildId
+                return@map stackFrame
+            }
+
+            val bugsnagThread = BugsnagThread(
+                thread.id.toString(),
+                thread.name,
+                ErrorType.C,
+                false,
+                State.UNKNOWN,
+                logger
+            )
+            bugsnagThread.stacktrace = stackFrames
+            threadConsumer(bugsnagThread)
         }
     }
 }
