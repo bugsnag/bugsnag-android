fe2850a7561cd7501844daa2f89f799e49918d25
YingYing Chen <129767130+SmartbearYing@users.noreply.github.com>
2024-02-13 14:09:09 +0000

    feat(NDK)Device info decoder

--- bugsnag-plugin-android-ndk/src/test/java/com/bugsnag/android/NativeEventDecoder32bitTest.kt
+++ bugsnag-plugin-android-ndk/src/test/java/com/bugsnag/android/NativeEventDecoder32bitTest.kt
@@ -13,7 +13,6 @@ import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
-import java.util.Date
 
 @RunWith(MockitoJUnitRunner::class)
 class NativeEventDecoder32bitTest {
@@ -23,7 +22,6 @@ class NativeEventDecoder32bitTest {
     private val event = mock(Event::class.java)
     private val session = mock(Session::class.java)
     private val notifier = mock(Notifier::class.java)
-    private val device = mock(DeviceWithState::class.java)
 
     @Before
     fun setupArchitecture() {
@@ -40,15 +38,15 @@ class NativeEventDecoder32bitTest {
         val data = ByteBuffer.wrap(crashDumpData)
         data.order(ByteOrder.LITTLE_ENDIAN)
         val captor = ArgumentCaptor.forClass(AppWithState::class.java)
-        val runtimeVersions = mutableMapOf<String, Any>()
+//        val deviceCaptor = ArgumentCaptor.forClass(DeviceWithState::class.java)
         `when`(event.session).thenReturn(session)
         `when`(session.notifier).thenReturn(notifier)
-        `when`(event.device).thenReturn(device)
-        `when`(device.runtimeVersions).thenReturn(runtimeVersions)
 
         NativeEventDecoder.decodeEventFromBytes(data, event)
 
         verify(event).app = captor.capture()
+
+//        verify(event).device = deviceCaptor.capture()
 
         verify(notifier).name = ""
         verify(notifier).version = ""
@@ -66,17 +64,7 @@ class NativeEventDecoder32bitTest {
         assertEquals(true, captor.value.isLaunching)
         assertEquals("arm32", captor.value.binaryArch)
 
-        assertEquals(15, runtimeVersions["apiLevel"])
-        assertEquals("6.7.3-94_SPI-324", runtimeVersions["osBuild"])
-        verify(device).orientation = "portrait"
-        verify(device).time = Date(1339585022 * 1000L)
-        verify(device).id = "deddc379-5a77-4f2c-b21c-b24baca697f6"
-        verify(device).jailbroken = false
-        verify(device).locale = "en_GB"
-        verify(device).manufacturer = "motorola"
-        verify(device).model = "XT910"
-        verify(device).osVersion = "4.0.4"
-        verify(device).osName = "android"
-        verify(device).totalMemory = 0L
+//        assertEquals("arm32", deviceCaptor.value.cpuAbi)
+
     }
 }
