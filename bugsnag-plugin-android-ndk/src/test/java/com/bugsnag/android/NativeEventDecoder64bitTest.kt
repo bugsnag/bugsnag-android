c75daedbbe1447cadf441b55bdef110838f9bf01
SmartbearYing <yingying.chen@smartbear.com>
2024-02-15 16:53:46 +0000

    feat(NDK)User info decoder

--- bugsnag-plugin-android-ndk/src/test/java/com/bugsnag/android/NativeEventDecoder64bitTest.kt
+++ bugsnag-plugin-android-ndk/src/test/java/com/bugsnag/android/NativeEventDecoder64bitTest.kt
@@ -7,12 +7,14 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.ArgumentCaptor
+import org.mockito.Mockito
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
+import java.util.Date
 
 @RunWith(MockitoJUnitRunner::class)
 class NativeEventDecoder64bitTest {
@@ -22,9 +24,18 @@ class NativeEventDecoder64bitTest {
     private val event = mock(Event::class.java)
     private val session = mock(Session::class.java)
     private val notifier = mock(Notifier::class.java)
+    private val device = mock(DeviceWithState::class.java)
+
+    lateinit var data: ByteBuffer
 
     @Before
-    fun setupArchitecture() {
+    fun setUp() {
+        setupArchitecture()
+        data = ByteBuffer.wrap(crashDump64BitData)
+        data.order(ByteOrder.LITTLE_ENDIAN)
+    }
+
+    private fun setupArchitecture() {
         NativeArch._is32Bit = false
     }
 
@@ -34,20 +45,27 @@ class NativeEventDecoder64bitTest {
     }
 
     @Test
-    fun testNativeEventDecode() {
-        val data = ByteBuffer.wrap(crashDump64BitData)
-        data.order(ByteOrder.LITTLE_ENDIAN)
-        val captor = ArgumentCaptor.forClass(AppWithState::class.java)
+    fun testNativeNotifierDecode() {
         `when`(event.session).thenReturn(session)
         `when`(session.notifier).thenReturn(notifier)
+        `when`(event.device).thenReturn(device)
 
         NativeEventDecoder.decodeEventFromBytes(data, event)
-
-        verify(event).app = captor.capture()
 
         verify(notifier).name = ""
         verify(notifier).version = ""
         verify(notifier).url = ""
+    }
+
+    @Test
+    fun testAppInfoDecode() {
+        `when`(event.session).thenReturn(session)
+        `when`(session.notifier).thenReturn(notifier)
+        `when`(event.device).thenReturn(device)
+        val captor = ArgumentCaptor.forClass(AppWithState::class.java)
+
+        NativeEventDecoder.decodeEventFromBytes(data, event)
+        verify(event).app = captor.capture()
 
         assertEquals("com.example.bugsnag.android", captor.value.id)
         assertEquals("production", captor.value.releaseStage)
@@ -60,5 +78,44 @@ class NativeEventDecoder64bitTest {
         assertEquals(true, captor.value.inForeground)
         assertEquals(true, captor.value.isLaunching)
         assertEquals("arm64", captor.value.binaryArch)
+    }
+
+    @Test
+    fun testDeviceInfoDecode() {
+        val captor = ArgumentCaptor.forClass(AppWithState::class.java)
+        val runtimeVersions = mutableMapOf<String, Any>()
+        `when`(event.session).thenReturn(session)
+        `when`(session.notifier).thenReturn(notifier)
+        `when`(event.device).thenReturn(device)
+        `when`(device.runtimeVersions).thenReturn(runtimeVersions)
+
+        NativeEventDecoder.decodeEventFromBytes(data, event)
+        verify(event).app = captor.capture()
+
+        assertEquals(34, runtimeVersions["apiLevel"])
+        assertEquals("UE1A.230829.030", runtimeVersions["osBuild"])
+        verify(device).orientation = "portrait"
+        verify(device).time = Date(1706172734 * 1000L)
+        verify(device).id = "dba44d63-31d6-4770-9617-8d8782213d23"
+        verify(device).jailbroken = false
+        verify(device).locale = "en_US"
+        verify(device).manufacturer = "Google"
+        verify(device).model = "sdk_gphone64_arm64"
+        verify(device).osVersion = "14"
+        verify(device).osName = "android"
+        verify(device).totalMemory = 0L
+    }
+
+    @Test
+    fun testUserInfoDecode() {
+        val runtimeVersions = mutableMapOf<String, Any>()
+        `when`(event.session).thenReturn(session)
+        `when`(session.notifier).thenReturn(notifier)
+        `when`(event.device).thenReturn(device)
+        `when`(device.runtimeVersions).thenReturn(runtimeVersions)
+
+        NativeEventDecoder.decodeEventFromBytes(data, event)
+
+        verify(event, Mockito.times(1)).setUser("999999", "ndk override", "j@ex.co")
     }
 }
