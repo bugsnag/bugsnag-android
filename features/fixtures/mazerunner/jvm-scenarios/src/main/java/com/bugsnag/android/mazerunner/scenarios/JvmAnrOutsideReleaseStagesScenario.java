ec6b0de685377b4d8c762584df080c8406d7965e
Steve Kirkland-Walton <steve@bugsnag.com>
2024-03-20 17:04:12 +0000

    Allow for the app already having stopped [full ci]

--- features/fixtures/mazerunner/jvm-scenarios/src/main/java/com/bugsnag/android/mazerunner/scenarios/JvmAnrOutsideReleaseStagesScenario.java
+++ features/fixtures/mazerunner/jvm-scenarios/src/main/java/com/bugsnag/android/mazerunner/scenarios/JvmAnrOutsideReleaseStagesScenario.java
@@ -3,7 +3,6 @@ package com.bugsnag.android.mazerunner.scenarios;
 import static com.bugsnag.android.mazerunner.AnrHelperKt.createDeadlock;
 import static com.bugsnag.android.mazerunner.LogKt.getZeroEventsLogMessages;
 
-
 import com.bugsnag.android.Configuration;
 
 import android.content.Context;
