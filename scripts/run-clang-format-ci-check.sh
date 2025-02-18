b2e976954b81f10906962fa7ed0a87c7e56f604f
Josh Edney <j.edney17@gmail.com>
2025-02-18 15:15:23 +0000

    print clang-format version

--- scripts/run-clang-format-ci-check.sh
+++ scripts/run-clang-format-ci-check.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+clang-format --version
+
 find bugsnag-plugin-android-anr/src/main -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --dry-run -Werror &&
 find bugsnag-plugin-android-ndk/src/main -iname *.h -o -iname *.cpp -o -iname *.c | grep -v src/main/jni/external/ | grep -v src/main/jni/deps/ | xargs clang-format --dry-run -Werror &&
 find examples/sdk-app-example/app/src/main -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --dry-run -Werror
