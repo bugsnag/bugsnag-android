4019407c40817aa96581c6296c154ec04f4806f9
jason <lemnik@users.noreply.github.com>
2025-09-11 09:14:17 +0100

    feat(ndk): align native libs to 16kb pages

--- bugsnag-android-core/src/main/java/com/bugsnag/android/ClientComponentCallbacks.kt
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/ClientComponentCallbacks.kt
@@ -19,6 +19,7 @@ internal class ClientComponentCallbacks(
     }
 
     override fun onTrimMemory(level: Int) {
+        @Suppress("DEPRECATION")
         memoryCallback(level >= ComponentCallbacks2.TRIM_MEMORY_COMPLETE, level)
     }
 
