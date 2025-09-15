8959125da710069034c063fe8245c9ca590326fc
YYChen01988 <Chen.yingying.01988@gmail.com>
2025-09-15 17:30:44 +0100

    feat(remote config) unit test

--- bugsnag-android-core/src/main/java/com/bugsnag/android/RemoteConfig.kt
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/RemoteConfig.kt
@@ -1,38 +1,27 @@
 package com.bugsnag.android
 
 import android.util.JsonReader
+import com.bugsnag.android.RemoteConfig.Companion.DISCARD_RULE_ALL
+import com.bugsnag.android.RemoteConfig.Companion.DISCARD_RULE_ALL_HANDLED
+import com.bugsnag.android.RemoteConfig.Companion.MATCH_TYPE
 import com.bugsnag.android.internal.DateUtils
 import java.util.Date
 
 internal class RemoteConfig(
-    val configurationTag: String?,
-    val configurationExpiry: Date?,
+    val configurationTag: String,
+    val configurationExpiry: Date,
     val discardRules: List<DiscardRule>
 ) : JsonStream.Streamable {
 
     override fun toStream(stream: JsonStream) {
         stream.beginObject()
         stream.name(KEY_CONFIGURATION_TAG).value(configurationTag)
-        configurationExpiry?.let { stream.name(KEY_CONFIG_EXPIRY).value(DateUtils.toIso8601(it)) }
-        stream.name(KEY_DISCARD_RULES).beginArray()
-        for (rule in discardRules) {
-            stream.beginObject()
-            when (rule) {
-                is DiscardRule.All -> {
-                    stream.name(MATCH_TYPE).value(DISCARD_RULE_ALL)
-                }
-
-                is DiscardRule.AllHandled -> {
-                    stream.name(MATCH_TYPE).value(DISCARD_RULE_ALL_HANDLED)
-                }
-            }
-            stream.endObject()
-        }
-        stream.endArray()
+        stream.name(KEY_CONFIG_EXPIRY).value(DateUtils.toIso8601(configurationExpiry))
+        stream.name(KEY_DISCARD_RULES).value(discardRules)
         stream.endObject()
     }
 
-    internal companion object : JsonReadable<RemoteConfig> {
+    internal companion object : JsonReadable<RemoteConfig?> {
         internal const val KEY_CONFIGURATION_TAG = "configurationTag"
         internal const val KEY_CONFIG_EXPIRY = "configurationExpiry"
         internal const val KEY_DISCARD_RULES = "discardRules"
@@ -40,79 +29,88 @@ internal class RemoteConfig(
         internal const val DISCARD_RULE_ALL_HANDLED = "ALL_HANDLED"
         internal const val MATCH_TYPE = "match_type"
 
-        override fun fromReader(reader: JsonReader): RemoteConfig {
-            var remoteConfig: RemoteConfig
-            with(reader) {
-                beginObject()
-                var configurationTag: String = ""
-                var configExpiry: Date = Date(0)
-                var discardRules: List<DiscardRule> = emptyList()
+        override fun fromReader(reader: JsonReader): RemoteConfig? {
+            var configurationTag: String? = null
+            var configurationExpiry: Date? = null
+            var discardRules: List<DiscardRule> = emptyList()
+            reader.beginObject()
+            while (reader.hasNext()) {
+                val key = reader.nextName()
+                when (key) {
+                    KEY_CONFIGURATION_TAG -> configurationTag = reader.nextString()
+                    KEY_CONFIG_EXPIRY ->
+                        configurationExpiry = DateUtils.fromIso8601(reader.nextString())
 
-                while (hasNext()) {
-                    val key = nextName()
-                    when (key) {
-                        KEY_CONFIGURATION_TAG -> configurationTag = nextString()
-                        KEY_CONFIG_EXPIRY -> configExpiry = DateUtils.fromIso8601(nextString())
-                        KEY_DISCARD_RULES -> {
-                            discardRules = parseInternals(reader)
-                        }
-                    }
+                    KEY_DISCARD_RULES -> discardRules = parseDiscardRules(reader)
                 }
-                remoteConfig = RemoteConfig(configurationTag, configExpiry, discardRules)
-                endObject()
             }
-            return remoteConfig
+            reader.endObject()
+
+            return if (configurationTag != null && configurationExpiry != null) {
+                RemoteConfig(
+                    configurationTag,
+                    configurationExpiry,
+                    discardRules
+                )
+            } else {
+                null
+            }
         }
 
-        private fun parseInternals(reader: JsonReader): List<DiscardRule> {
+        private fun parseDiscardRules(reader: JsonReader): List<DiscardRule> {
             val rules = ArrayList<DiscardRule>()
-            with(reader) {
-                beginArray()
-                while (hasNext()) {
-                    val parsedRule = parseDiscardRule(reader)
-                    parsedRule?.let { rules.add(it) }
-                }
-                endArray()
+            reader.beginArray()
+            while (reader.hasNext()) {
+                val rule = DiscardRule.fromReader(reader)
+                rule?.let { rules.add(it) }
             }
+            reader.endArray()
             return rules
-        }
-
-        private fun parseDiscardRule(reader: JsonReader): DiscardRule? {
-            var rule: DiscardRule? = null
-            with(reader) {
-                beginObject()
-                beginArray()
-                while (hasNext()) {
-                    beginObject()
-                    while (hasNext()) {
-                        val key = nextName()
-                        when (key) {
-                            MATCH_TYPE -> {
-                                when (nextString()) {
-                                    DISCARD_RULE_ALL -> rule = DiscardRule.All()
-                                    DISCARD_RULE_ALL_HANDLED -> rule = DiscardRule.AllHandled()
-                                }
-                            }
-                        }
-                        endObject()
-                    }
-                    endObject()
-                }
-                endArray()
-                endObject()
-            }
-            return rule
         }
     }
 }
 
-internal abstract class DiscardRule {
+internal sealed class DiscardRule : JsonStream.Streamable {
+    companion object : JsonReadable<DiscardRule?> {
+        override fun fromReader(reader: JsonReader): DiscardRule? {
+            var rule: DiscardRule? = null
+
+            reader.beginObject()
+            while (reader.hasNext()) {
+                val key = reader.nextName()
+                when (key) {
+                    MATCH_TYPE -> {
+                        when (reader.nextString()) {
+                            DISCARD_RULE_ALL -> rule = All()
+                            DISCARD_RULE_ALL_HANDLED -> rule = AllHandled()
+                        }
+                    }
+                }
+            }
+            reader.endObject()
+            return rule
+        }
+    }
+
     abstract fun shouldDiscard(payload: EventPayload): Boolean
     class All : DiscardRule() {
         override fun shouldDiscard(payload: EventPayload): Boolean = true
+        override fun toStream(stream: JsonStream) {
+            stream.beginObject()
+            stream.name(MATCH_TYPE).value(DISCARD_RULE_ALL)
+            stream.endObject()
+        }
     }
 
     class AllHandled : DiscardRule() {
-        override fun shouldDiscard(payload: EventPayload): Boolean = true
+        override fun shouldDiscard(payload: EventPayload): Boolean {
+            return !(payload.event?.isUnhandled ?: true)
+        }
+
+        override fun toStream(stream: JsonStream) {
+            stream.beginObject()
+            stream.name(MATCH_TYPE).value(DISCARD_RULE_ALL_HANDLED)
+            stream.endObject()
+        }
     }
 }
--- bugsnag-android-core/src/test/java/com/bugsnag/android/RemoteConfigSerializationTest.kt
+++ bugsnag-android-core/src/test/java/com/bugsnag/android/RemoteConfigSerializationTest.kt
@@ -6,17 +6,11 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameter
 import org.junit.runners.Parameterized.Parameters
-import java.util.Date
 
 @RunWith(Parameterized::class)
 internal class RemoteConfigSerializationTest {
 
     companion object {
-
-        val allRule: DiscardRule = DiscardRule.All()
-        val allHandledRule: DiscardRule = DiscardRule.AllHandled()
-        val dateString = "2024-01-15T10:30:45.123Z"
-        val date = DateUtils.fromIso8601(dateString)
 
         @JvmStatic
         @Parameters
@@ -24,15 +18,20 @@ internal class RemoteConfigSerializationTest {
             "remoteconfig",
             RemoteConfig(
                 "tag123",
-                date,
-                listOf(allRule, allHandledRule)
+                DateUtils.fromIso8601("2024-01-15T10:30:45.123Z"),
+                listOf(DiscardRule.All(), DiscardRule.AllHandled())
+            ),
+            RemoteConfig(
+                "tag456",
+                DateUtils.fromIso8601("2024-01-15T10:30:45.123Z"),
+                emptyList()
             )
         )
     }
 
     @Parameter
-    lateinit var testCase: Pair<User, String>
+    lateinit var testCase: Pair<RemoteConfig, String>
 
     @Test
-    fun testJsonSerialisation() = verifyJsonMatches(testCase.first, testCase.second)
+    fun testJsonSerialization() = verifyJsonMatches(testCase.first, testCase.second)
 }
