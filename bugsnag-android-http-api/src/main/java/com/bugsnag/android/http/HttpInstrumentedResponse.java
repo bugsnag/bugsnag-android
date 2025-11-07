6ab017fca282575a9779c24366d806222961d5a8
jason <lemnik@users.noreply.github.com>
2025-11-07 15:20:47 +0000

    doc(http): documentation comments for the new HTTP instrumentation API

--- bugsnag-android-http-api/src/main/java/com/bugsnag/android/http/HttpInstrumentedResponse.java
+++ bugsnag-android-http-api/src/main/java/com/bugsnag/android/http/HttpInstrumentedResponse.java
@@ -3,15 +3,79 @@ package com.bugsnag.android.http;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+/**
+ * Represents an HTTP response that has been instrumented by BugSnag. This interface provides
+ * access to the original request and response objects, as well as methods to modify the
+ * information that is reported to BugSnag.
+ *
+ * @param <R> the request type of the HTTP API being instrumented
+ * @param <S> the response type of the HTTP API being instrumented
+ */
 public interface HttpInstrumentedResponse<R, S> {
+    /**
+     * The original HTTP request object.
+     *
+     * @return the original request
+     */
     @NonNull
     R getRequest();
 
+    /**
+     * The original HTTP response object, if one was received. This may be {@code null} if the
+     * request failed to produce a response (e.g. due to a network error).
+     *
+     * @return the original response, or {@code null}
+     */
     @Nullable
     S getResponse();
 
+    /**
+     * Override whether this request/response should be reported as a breadcrumb. This defaults
+     * to the value passed to {@link HttpInstrumentationBuilder#logBreadcrumbs(boolean)}, but
+     * cannot override whether {@link com.bugsnag.android.BreadcrumbType#REQUEST REQUEST}
+     * breadcrumbs are enabled.
+     *
+     * @param isBreadcrumbReported false if a breadcrumb should not be reported
+     */
+    void setBreadcrumbReported(boolean isBreadcrumbReported);
+
+    /**
+     * Check whether a breadcrumb will be reported for this response.
+     *
+     * @return true if a breadcrumb will be reported
+     */
+    boolean isBreadcrumbReported();
+
+    /**
+     * Mark that an error should be reported for this response. This will default to true if the
+     * HTTP status code in the {@link #getResponse() response} was added as an "error code" using
+     * {@link HttpInstrumentationBuilder#addHttpErrorCode(int)}.
+     *
+     * @param isErrorReported true if an error has been reported
+     */
+    void setErrorReported(boolean isErrorReported);
+
+    /**
+     * Check whether an error will be reported for this response.
+     *
+     * @return true if an error should be reported
+     */
+    boolean isErrorReported();
+
+    /**
+     * The response body that will be reported to BugSnag for this response. This may be different
+     * from the original response body if it has been modified by a callback.
+     *
+     * @return the reported response body
+     */
     @Nullable
     String getReportedResponseBody();
 
+    /**
+     * Set the response body that will be reported to BugSnag for this response. Setting this to
+     * {@code null} will prevent the response body from being reported.
+     *
+     * @param responseBody the response body to report
+     */
     void setReportedResponseBody(@Nullable String responseBody);
 }
--- bugsnag-android-http-api/src/main/java/com/bugsnag/android/http/HttpRequestCallback.java
+++ bugsnag-android-http-api/src/main/java/com/bugsnag/android/http/HttpRequestCallback.java
@@ -2,6 +2,19 @@ package com.bugsnag.android.http;
 
 import androidx.annotation.NonNull;
 
+/**
+ * A callback that is invoked before an HTTP request is sent. This can be used to modify the
+ * request information that is reported to BugSnag, or to prevent the request from being
+ * reported at all.
+ *
+ * @param <R> the request type of the HTTP API being instrumented
+ * @see HttpInstrumentationBuilder#addRequestCallback(HttpRequestCallback)
+ */
 public interface HttpRequestCallback<R> {
+    /**
+     * Called before an HTTP request is sent.
+     *
+     * @param req the instrumented request
+     */
     void onHttpRequest(@NonNull HttpInstrumentedRequest<R> req);
 }
