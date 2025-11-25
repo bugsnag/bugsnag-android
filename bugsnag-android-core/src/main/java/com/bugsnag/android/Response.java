4c6b4e867eb7a4099a75ff2c175c4796c12d362a
jason <lemnik@users.noreply.github.com>
2025-11-25 16:22:07 +0000

    feat(http): added Request/Response as optional properties on an Event

---
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/Response.java
@@ -0,0 +1,42 @@
+package com.bugsnag.android;
+
+import androidx.annotation.IntRange;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+/**
+ * A Response represents an HTTP response that forms part of an {@link Event}.
+ */
+public final class Response extends AbstractHttpEntity implements JsonStream.Streamable {
+    private int statusCode;
+
+    Response(@IntRange(from = 0) int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(@IntRange(from = 0) int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    @Override
+    public void toStream(@NotNull JsonStream writer) throws IOException {
+        writer.beginObject();
+        writer.name("statusCode").value(statusCode);
+
+        writer.name("body").value(getBody());
+
+        long bodyLength = getBodyLength();
+        if (bodyLength >= 0) {
+            writer.name("bodyLength").value(bodyLength);
+        }
+
+        writer.name("headers").value(headers, true);
+        writer.endObject();
+    }
+}
