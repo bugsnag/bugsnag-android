37d8be86535c1c9e663b4dd58f1de33353d1cbcc
jason <lemnik@users.noreply.github.com>
2025-12-02 16:38:08 +0000

    test(ErrorCaptureOptions): added a test for all possible bit fields used in `captureOnly`

--- bugsnag-android-core/src/test/java/com/bugsnag/android/ErrorCaptureOptionsTest.kt
+++ bugsnag-android-core/src/test/java/com/bugsnag/android/ErrorCaptureOptionsTest.kt
@@ -1,5 +1,10 @@
 package com.bugsnag.android
 
+import com.bugsnag.android.ErrorCaptureOptions.Companion.CAPTURE_BREADCRUMBS
+import com.bugsnag.android.ErrorCaptureOptions.Companion.CAPTURE_FEATURE_FLAGS
+import com.bugsnag.android.ErrorCaptureOptions.Companion.CAPTURE_STACKTRACE
+import com.bugsnag.android.ErrorCaptureOptions.Companion.CAPTURE_THREADS
+import com.bugsnag.android.ErrorCaptureOptions.Companion.CAPTURE_USER
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNull
@@ -21,7 +26,7 @@ class ErrorCaptureOptionsTest {
 
     @Test
     fun testCaptureOnlyStacktraceAndUser() {
-        val fields = ErrorCaptureOptions.CAPTURE_STACKTRACE or ErrorCaptureOptions.CAPTURE_USER
+        val fields = CAPTURE_STACKTRACE or CAPTURE_USER
         val options = ErrorCaptureOptions.captureOnly(fields)
         assertTrue(options.stacktrace)
         assertFalse(options.breadcrumbs)
@@ -33,7 +38,7 @@ class ErrorCaptureOptionsTest {
 
     @Test
     fun testCaptureOnlyWithMetadata() {
-        val fields = ErrorCaptureOptions.CAPTURE_BREADCRUMBS or ErrorCaptureOptions.CAPTURE_FEATURE_FLAGS
+        val fields = CAPTURE_BREADCRUMBS or CAPTURE_FEATURE_FLAGS
         val metadataTabs = setOf("customTab")
         val options = ErrorCaptureOptions.captureOnly(fields, metadataTabs)
         assertFalse(options.stacktrace)
@@ -53,5 +58,45 @@ class ErrorCaptureOptionsTest {
         assertFalse(options.threads)
         assertFalse(options.user)
         assertEquals(emptySet<String>(), options.metadata)
+    }
+
+    @Test
+    fun captureOnlyBitFields() {
+        val bitMappings = mapOf<Int, (ErrorCaptureOptions) -> Boolean>(
+            CAPTURE_STACKTRACE to ErrorCaptureOptions::stacktrace,
+            CAPTURE_BREADCRUMBS to ErrorCaptureOptions::breadcrumbs,
+            CAPTURE_FEATURE_FLAGS to ErrorCaptureOptions::featureFlags,
+            CAPTURE_THREADS to ErrorCaptureOptions::threads,
+            CAPTURE_USER to ErrorCaptureOptions::user,
+        )
+
+        // test every possible combination of bit fields -> Boolean mappings
+        val allBits = bitMappings.keys.toIntArray()
+        val totalCombinations = 1 shl allBits.size
+
+        for (combination in 0 until totalCombinations) {
+            var fields = 0
+            val expectedEnabled = mutableSetOf<Int>()
+
+            // Build the bit field based on the combination
+            for (i in allBits.indices) {
+                if ((combination and (1 shl i)) != 0) {
+                    fields = fields or allBits[i]
+                    expectedEnabled.add(allBits[i])
+                }
+            }
+
+            val options = ErrorCaptureOptions.captureOnly(fields)
+
+            // Verify each field matches expectations
+            for ((bit, getter) in bitMappings) {
+                val shouldBeEnabled = bit in expectedEnabled
+                assertEquals(
+                    "Combination $combination: bit $bit should be $shouldBeEnabled",
+                    shouldBeEnabled,
+                    getter(options)
+                )
+            }
+        }
     }
 }
