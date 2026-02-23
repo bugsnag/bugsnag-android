98d7d07e84b24e0cb6046e0d131a78635d2b5d8c
jason <lemnik@users.noreply.github.com>
2026-02-23 11:43:50 +0000

    test(CXXMarkLaunchCompletedScenario): changed CXXMarkLaunchCompletedScenario to use OnSendCallback instead of InterceptingDelivery

--- features/fixtures/mazerunner/cxx-scenarios/src/main/java/com/bugsnag/android/mazerunner/scenarios/CXXMarkLaunchCompletedScenario.kt
+++ features/fixtures/mazerunner/cxx-scenarios/src/main/java/com/bugsnag/android/mazerunner/scenarios/CXXMarkLaunchCompletedScenario.kt
@@ -1,11 +1,10 @@
 package com.bugsnag.android.mazerunner.scenarios
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import com.bugsnag.android.Bugsnag
 import com.bugsnag.android.Configuration
-import com.bugsnag.android.createDefaultDelivery
-import com.bugsnag.android.mazerunner.InterceptingDelivery
-import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Sends an NDK error to Bugsnag after markLaunchCompleted() is invoked.
@@ -16,19 +15,18 @@ internal class CXXMarkLaunchCompletedScenario(
     eventMetadata: String?
 ) : Scenario(config, context, eventMetadata) {
 
-    private val deliveryCount = AtomicInteger(0)
-
     external fun crash()
 
     init {
         config.launchDurationMillis = 0
         System.loadLibrary("cxx-scenarios")
 
-        // wait for Bugsnag.notify() to complete before triggering NDK crash
-        config.delivery = InterceptingDelivery(createDefaultDelivery()) {
-            if (deliveryCount.incrementAndGet() == 1) {
+        config.addOnSend { _ ->
+            Handler(Looper.getMainLooper()).post {
                 crash()
             }
+
+            true
         }
     }
 
