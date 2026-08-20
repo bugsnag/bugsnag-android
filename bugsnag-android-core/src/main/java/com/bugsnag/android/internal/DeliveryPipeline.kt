56be527d7d8a89ab66f4adb06460b67c702c48cb
SB-sachinp <sachin.patil@smartbear.com>
2026-08-20 10:26:42 +0530

    Resolve conflict with next

--- bugsnag-android-core/src/main/java/com/bugsnag/android/internal/DeliveryPipeline.kt
+++ bugsnag-android-core/src/main/java/com/bugsnag/android/internal/DeliveryPipeline.kt
@@ -15,44 +15,56 @@ internal class DeliveryPipeline(
     private val logger: Logger get() = config.logger
 
     fun deliverEventPayload(payload: EventPayload): DeliveryStatus? {
-        try {
-            val retain = onSendCallbackState.runOnSendTasks(
-                { payload.event!! },
-                config.logger
-            )
-
-            if (!retain) {
-                return null
-            }
-        } catch (_: Exception) {
-            // most likely the payload could not be decoded, so we continue
+        if (!retainPayload(payload)) {
+            return null
         }
 
-        try {
-            val remoteConfig = getRemoteConfig()
-            if (remoteConfig != null) {
-                val event = payload.event
-                val isTimeSensitive = payload.isLaunchCrash || event?.errors?.any {
-                    it.errorClass == "ANR" || it.errorClass == "AppHang"
-                } ?: false
-
-                if (!isTimeSensitive) {
-                    val discardRules = remoteConfig.discardRules
-                    val applicableDiscardRule = discardRules.firstOrNull { it.shouldDiscard(payload) }
-                    if (applicableDiscardRule != null) {
-                        logger.d("Discarding event due to remote discardRule: $applicableDiscardRule")
-                        // discarded events are treated as being delivered, as the server would have discarded them
-                        return DeliveryStatus.DELIVERED
-                    }
-                }
-            }
-        } catch (_: Exception) {
-            // swallow any RemoteConfig related errors, and favour delivering the payload
+        getDiscardStatus(payload)?.let {
+            return it
         }
 
         val deliveryParams = config.getErrorApiDeliveryParams(payload)
         val delivery = config.delivery
         return delivery.deliver(payload, deliveryParams)
+    }
+
+    private fun retainPayload(payload: EventPayload): Boolean {
+        return try {
+            onSendCallbackState.runOnSendTasks(
+                { payload.event!! },
+                config.logger
+            )
+        } catch (_: Exception) {
+            // most likely the payload could not be decoded, so we continue
+            true
+        }
+    }
+
+    private fun getDiscardStatus(payload: EventPayload): DeliveryStatus? {
+        return try {
+            val remoteConfig = getRemoteConfig() ?: return null
+            if (isTimeSensitive(payload)) {
+                return null
+            }
+
+            val applicableDiscardRule = remoteConfig.discardRules.firstOrNull {
+                it.shouldDiscard(payload)
+            } ?: return null
+
+            logger.d("Discarding event due to remote discardRule: $applicableDiscardRule")
+            // discarded events are treated as being delivered, as the server would have discarded them
+            DeliveryStatus.DELIVERED
+        } catch (_: Exception) {
+            // swallow any RemoteConfig related errors, and favour delivering the payload
+            null
+        }
+    }
+
+    private fun isTimeSensitive(payload: EventPayload): Boolean {
+        val event = payload.event
+        return payload.isLaunchCrash || payload.isUnhandled || event?.errors?.any {
+            it.errorClass == "ANR" || it.errorClass == "AppHang"
+        } ?: false
     }
 
     private fun getRemoteConfig(): RemoteConfig? {
